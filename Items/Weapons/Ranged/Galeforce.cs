abcac387cf6f2cecda5fbdfcc4b8fd3fe0d1d023
AquaSG <macvine9@gmail.com>
2023-08-26 00:50:55 -0400

    wip galeforce

--- Items/Weapons/Ranged/Galeforce.cs
+++ Items/Weapons/Ranged/Galeforce.cs
@@ -5,6 +5,8 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using CalamityMod.Projectiles.Magic;
+using System.Linq;
 
 namespace CalamityMod.Items.Weapons.Ranged
 {
@@ -15,31 +17,55 @@ namespace CalamityMod.Items.Weapons.Ranged
         {
             Item.damage = 8;
             Item.DamageType = DamageClass.Ranged;
-            Item.width = 32;
-            Item.height = 52;
+            Item.width = 38;
+            Item.height = 68;
             Item.useTime = 20;
             Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
+            Item.channel = true;
             Item.knockBack = 3f;
             Item.value = CalamityGlobalItem.Rarity3BuyPrice;
             Item.rare = ItemRarityID.Orange;
-            Item.UseSound = SoundID.Item5;
+            Item.UseSound = SoundID.Item17;
             Item.autoReuse = true;
-            Item.shoot = ProjectileID.WoodenArrowFriendly;
+            Item.shoot = ModContent.ProjectileType<GaleforceHoldout>();
             Item.shootSpeed = 20f;
-            Item.useAmmo = AmmoID.Arrow;
+            //Item.useAmmo = AmmoID.Arrow;
             Item.Calamity().canFirePointBlankShots = true;
         }
-
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        public override void HoldItem(Player player)
         {
-            for (int i = -8; i <= 8; i += 8)
+            player.Calamity().mouseWorldListener = true;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return !Main.projectile.Any(n => n.active && n.owner == player.whoAmI && n.type == ModContent.ProjectileType<GaleforceHoldout>());
+        }
+
+        public override void UseItemFrame(Player player)
+        {
+            //Calculate the dirction in which the players arms should be pointing at.
+            Vector2 playerToCursor = (player.Calamity().mouseWorld - player.Center).SafeNormalize(Vector2.UnitX);
+            float armPointingDirection = (playerToCursor.ToRotation());
+
+            player.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, armPointingDirection - MathHelper.PiOver2);
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, armPointingDirection - MathHelper.PiOver2);
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            if (player.Calamity().mouseWorld.X > player.Center.X)
             {
-                Vector2 perturbedSpeed = velocity.RotatedBy(MathHelper.ToRadians(i));
-                Projectile.NewProjectile(source, position, perturbedSpeed, ModContent.ProjectileType<FeatherLarge>(), damage / 4, 0f, player.whoAmI);
+                player.ChangeDir(1);
             }
-            return true;
+            else
+            {
+                player.ChangeDir(-1);
+            }
+
+            CalamityUtils.CleanHoldStyle(player, player.compositeFrontArm.rotation + MathHelper.PiOver2, player.GetFrontHandPosition(player.compositeFrontArm.stretch, player.compositeFrontArm.rotation).Floor(), new Vector2(38, 68), new Vector2(-9, 0));
         }
 
         public override void AddRecipes()
---
+++ Projectiles/Ranged/GaleforceArrow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Dusts;
+using CalamityMod.Items.Weapons.Ranged;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Projectiles.Ranged
+{
+    public class GaleforceArrow : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Ranged";
+
+        public override void SetStaticDefaults()
+        {
+            ProjectileID.Sets.TrailCacheLength[Projectile.type] = 10;
+            ProjectileID.Sets.TrailingMode[Projectile.type] = 1;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 10;
+            Projectile.height = 10;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.arrow = true;
+            Projectile.penetrate = 1;
+            Projectile.timeLeft = 300;
+            Projectile.Calamity().pointBlankShotDuration = CalamityGlobalProjectile.DefaultPointBlankDuration;
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Collision.HitTiles(Projectile.position, Projectile.velocity, Projectile.width, Projectile.height);
+            SoundEngine.PlaySound(SoundID.Dig, Projectile.position);
+            return true;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = (float)Math.Atan2((double)Projectile.velocity.Y, (double)Projectile.velocity.X) - MathHelper.ToRadians(90);
+
+            Vector2 destination = Projectile.Center;
+            bool locatedTarget = false;
+
+            // Find a target.
+            for (int i = 0; i < Main.maxNPCs; i++)
+            {
+                float extraDistance = (Main.npc[i].width / 2) + (Main.npc[i].height / 2);
+                if (!Main.npc[i].CanBeChasedBy(Projectile, false) || !Projectile.WithinRange(Main.npc[i].Center, 340f + extraDistance))
+                    continue;
+
+                Vector2 coneHomeCheck = (Main.npc[i].Center - Projectile.Center).SafeNormalize(Vector2.Zero) + Projectile.velocity.SafeNormalize(Vector2.Zero);
+                if (Collision.CanHit(Projectile.Center, 1, 1, Main.npc[i].Center, 1, 1) && (Math.Abs(coneHomeCheck.X) > 1.15f || Math.Abs(coneHomeCheck.Y) > 1.15f)) 
+                {
+                    destination = Main.npc[i].Center;
+                    locatedTarget = true;
+                    break;
+                }
+            }
+
+            if (locatedTarget)
+            {
+                // Home in on the target.
+                Vector2 homeDirection = (destination - Projectile.Center).SafeNormalize(Vector2.UnitY);
+                Projectile.velocity = (Projectile.velocity * 12f + homeDirection * 20f) / (12f + 1f);
+            }
+
+            if (Projectile.velocity.Length() < 20f)
+            {
+                Projectile.velocity = Projectile.velocity.SafeNormalize(Vector2.UnitY) * 20f;
+            }
+        }
+
+        public override void Kill(int timeLeft)
+        {
+
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            CalamityUtils.DrawAfterimagesCentered(Projectile, ProjectileID.Sets.TrailingMode[Projectile.type], lightColor, 2);
+            return false;
+        }
+    }
+}
