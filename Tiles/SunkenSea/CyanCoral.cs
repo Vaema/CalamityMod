834911e8709c970d568ade483ffbbfeeee6ef15b
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2023-11-05 10:42:37 -0700

    Corals for the reefs

---
+++ Tiles/SunkenSea/CyanCoral.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CalamityMod.Tiles.SunkenSea
+{
+    public class CyanCoral : ModTile
+    {
+        public byte[,] tileAdjacency;
+        public byte[,] secondTileAdjacency;
+        public byte[,] thirdTileAdjacency;
+        public byte[,] fourthTileAdjacency;
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            CalamityUtils.MergeWithGeneral(Type);
+
+            TileID.Sets.HasSlopeFrames[Type] = true;
+
+            TileID.Sets.ChecksForMerge[Type] = true;
+            HitSound = SoundID.Shatter;
+            DustType = 119;
+            AddMapEntry(new Color(204, 143, 174));
+            Main.tileShine2[Type] = true;
+
+            TileID.Sets.CanBeDugByShovel[Type] = true;
+
+            TileFraming.SetUpUniversalMerge(Type, ModContent.TileType<TimelessSand>(), out tileAdjacency);
+            TileFraming.SetUpUniversalMerge(Type, ModContent.TileType<Shellstone>(), out secondTileAdjacency);
+            TileFraming.SetUpUniversalMerge(Type, ModContent.TileType<EutrophicSand>(), out thirdTileAdjacency);
+            TileFraming.SetUpUniversalMerge(Type, ModContent.TileType<Navystone>(), out fourthTileAdjacency);
+        }
+
+        public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            //TileFraming.DrawUniversalMergeFrames(i, j, tileAdjacency, "CalamityMod/Tiles/Merges/TimelessSandMerge");
+            //TileFraming.DrawUniversalMergeFrames(i, j, secondTileAdjacency, "CalamityMod/Tiles/Merges/ShellstoneMerge");
+            TileFraming.DrawUniversalMergeFrames(i, j, thirdTileAdjacency, "CalamityMod/Tiles/Merges/EutrophicSandMerge");
+            TileFraming.DrawUniversalMergeFrames(i, j, fourthTileAdjacency, "CalamityMod/Tiles/Merges/NavystoneMerge");
+        }
+
+        public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+        {
+            TileFraming.GetAdjacencyData(i, j, ModContent.TileType<TimelessSand>(), out tileAdjacency[i, j]);
+            TileFraming.GetAdjacencyData(i, j, ModContent.TileType<Shellstone>(), out secondTileAdjacency[i, j]);
+            TileFraming.GetAdjacencyData(i, j, ModContent.TileType<EutrophicSand>(), out thirdTileAdjacency[i, j]);
+            TileFraming.GetAdjacencyData(i, j, ModContent.TileType<Navystone>(), out fourthTileAdjacency[i, j]);
+            return TileFraming.BetterGemsparkFraming(i, j, resetFrame);
+        }
+    }
+}
