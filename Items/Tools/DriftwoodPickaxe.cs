43d69b9de3e6ab9a1c117e86d8f8fa0253f1d5ef
Xyk <MetroidWhisperer@gmail.com>
2023-11-08 00:09:25 -0800

    3 wood sets of tools
    Added an Axe, Pickaxe, and Hammer for Acidwood, Driftwood, and Astral Monolith
    
    Sword and bow will come soon

---
+++ Items/Tools/DriftwoodPickaxe.cs
@@ -0,0 +1,55 @@
+﻿using CalamityMod.Items.Materials;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using CalamityMod.Items.Placeables;
+
+namespace CalamityMod.Items.Tools
+{
+    public class DriftwoodPickaxe : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Tools";
+        public override void SetDefaults()
+        {
+            Item.damage = 7;
+            Item.knockBack = 1.5f;
+            Item.useTime = 9;
+            Item.useAnimation = 18;
+            Item.pick = 55;
+
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 38;
+            Item.height = 38;
+            Item.useTurn = true;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.value = CalamityGlobalItem.Rarity0BuyPrice;
+            Item.rare = ItemRarityID.White;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+        }
+        public override void MeleeEffects(Player player, Rectangle hitbox)
+        {
+            bool surface = player.Center.Y < Main.worldSurface * 16.0;
+            bool GetEffects = ((Main.raining && surface) || player.dripping || (player.wet && !player.lavaWet && !player.honeyWet));
+            if (GetEffects)
+            {
+                int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 160);
+                Item.useTime = 7;
+                Item.useAnimation = 14;
+            }
+            else
+            {
+                Item.useTime = 9;
+                Item.useAnimation = 18;
+            }
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<Driftwood>(15).
+                AddTile(TileID.WorkBenches).
+                Register();
+        }
+    }
+}
