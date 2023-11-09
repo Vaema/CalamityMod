b019aa9e6b570457225fb22f122c1f8eeb042a49
Xyk <MetroidWhisperer@gmail.com>
2023-11-08 16:24:29 -0800

    Bows n' Swords
    Driftwood, Acidwood, and Monolith swords and bows

---
+++ Items/Weapons/Melee/AcidwoodSword.cs
@@ -0,0 +1,37 @@
+﻿using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Items.Materials;
+using Microsoft.Xna.Framework;
+using CalamityMod.Items.Placeables;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Weapons.Melee
+{
+    public class AcidwoodSword : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Weapons.Melee";
+        public override void SetDefaults()
+        {
+            Item.damage = 11;
+            Item.width = 36;
+            Item.height = 40;
+            Item.DamageType = DamageClass.Melee;
+            Item.useAnimation = Item.useTime = 18;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 3f;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.useTurn = true;
+            Item.value = CalamityGlobalItem.Rarity0BuyPrice;
+            Item.rare = ItemRarityID.White;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient<Acidwood>(7).
+                AddTile(TileID.WorkBenches).
+                Register();
+        }
+    }
+}
