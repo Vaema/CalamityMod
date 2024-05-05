22d97c1119d1e28617a88f1c0d4996edc778b1d8
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-05-05 17:13:58 -0400

    Start of Triploon revival
    * Launches three gravity-affected harpoons that stick into enemies and inflict Laceration
    * Releasing left click rips the harpoons out of struck enemies, which is a guaranteed crit and scales in damage based on player velocity
    * Triploon is used to craft Elemental Saw

--- Items/Weapons/Ranged/ElementalSaw.cs
+++ Items/Weapons/Ranged/ElementalSaw.cs
@@ -131,6 +131,7 @@ namespace CalamityMod.Items.Weapons.Ranged
             CreateRecipe().
                 AddIngredient<Buzzkill>().
                 AddIngredient<SpeedBlaster>().
+                AddIngredient<Triploon>().
                 AddIngredient(ItemID.LunarBar, 5).
                 AddIngredient<LifeAlloy>(5).
                 AddIngredient<GalacticaSingularity>(5).
