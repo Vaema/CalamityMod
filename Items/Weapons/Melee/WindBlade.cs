0dd381e62a1b8228cb980e9e9248cab5ffa11511
Xhack <72313282+energykid@users.noreply.github.com>
2024-05-21 10:54:40 -0400

    Custom use style items and projectiles; Wind Blade swing animations
    IMPORTANT NOTE - In order to use a projectile that extends BaseCustomUseStyleProjectile, you have to make the item that shoots it extend CustomUseProjItem.

--- Items/Weapons/Melee/WindBlade.cs
+++ Items/Weapons/Melee/WindBlade.cs
@@ -1,47 +1,38 @@
-﻿using Terraria.DataStructures;
+﻿using CalamityMod.Items.BaseItems;
 using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.BaseProjectiles;
 using CalamityMod.Projectiles.Melee;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.Melee
 {
-    public class WindBlade : ModItem, ILocalizedModType
+    public class WindBlade : CustomUseProjItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Melee";
         public override void SetDefaults()
         {
             Item.width = 58;
+            Item.height = 58;
             Item.damage = 41;
             Item.DamageType = DamageClass.Melee;
             Item.useAnimation = 20;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.useTime = 20;
             Item.useTurn = true;
-            Item.knockBack = 5;
-            Item.UseSound = SoundID.Item1;
+            Item.knockBack = 5f;
             Item.autoReuse = true;
-            Item.height = 58;
-            Item.value = CalamityGlobalItem.Rarity3BuyPrice;
+            Item.value = CalamityGlobalItem.RarityOrangeBuyPrice;
             Item.rare = ItemRarityID.Orange;
-            Item.shoot = ModContent.ProjectileType<Cyclone>();
-            Item.shootSpeed = 3f;
-        }
 
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-        {
-            Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage / 2, knockback, player.whoAmI, 0f, 0f);
-            return false;
-        }
-
-        public override void MeleeEffects(Player player, Rectangle hitbox)
-        {
-            if (Main.rand.NextBool(3))
-            {
-                int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, 59);
-            }
+            Item.channel = true;
+            Item.shoot = ModContent.ProjectileType<WindBladeHoldout>();
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.useStyle = ItemUseStyleID.Shoot;
         }
 
         public override void AddRecipes()
