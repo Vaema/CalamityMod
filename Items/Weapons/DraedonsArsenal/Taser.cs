2ba407e7386bc34fa924387388ef6abcc8fd3059
Xyk <MetroidWhisperer@gmail.com>
2024-05-26 01:35:57 -0700

    Taser

--- Items/Weapons/DraedonsArsenal/Taser.cs
+++ Items/Weapons/DraedonsArsenal/Taser.cs
@@ -5,7 +5,9 @@ using CalamityMod.Items.Materials;
 using CalamityMod.Items.Placeables;
 using CalamityMod.Projectiles.DraedonsArsenal;
 using CalamityMod.Sounds;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -14,27 +16,29 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
     public class Taser : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.DraedonsArsenal";
+        public static readonly SoundStyle Fire = new("CalamityMod/Sounds/Item/TaserLaunch") { Volume = 0.6f };
         public override void SetDefaults()
         {
             CalamityGlobalItem modItem = Item.Calamity();
 
-            Item.width = 50;
-            Item.height = 26;
+            Item.width = 42;
+            Item.height = 24;
             Item.DamageType = DamageClass.Ranged;
-            Item.damage = 15;
+            Item.damage = 20;
             Item.knockBack = 0f;
             Item.useTime = Item.useAnimation = 28;
             Item.autoReuse = true;
+            Item.reuseDelay = 15;
 
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.UseSound = CommonCalamitySounds.PlasmaBoltSound;
+            Item.UseSound = Fire;
             Item.noMelee = true;
 
             Item.value = CalamityGlobalItem.RarityOrangeBuyPrice;
             Item.rare = ItemRarityID.Orange;
 
             Item.shoot = ModContent.ProjectileType<TaserHook>();
-            Item.shootSpeed = 15f;
+            Item.shootSpeed = 6f;
 
             modItem.UsesCharge = true;
             modItem.MaxCharge = 50f;
@@ -43,8 +47,14 @@ namespace CalamityMod.Items.Weapons.DraedonsArsenal
 
         public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
 
+        public override Vector2? HoldoutOffset() => new Vector2(5f, 0f);
         public override void ModifyTooltips(List<TooltipLine> tooltips) => CalamityGlobalItem.InsertKnowledgeTooltip(tooltips, 1);
+        public override void UseItemFrame(Player player)
+        {
+            //float rotation = (player.Center - Item.).ToRotation() * player.gravDir + MathHelper.PiOver2;
 
+            //player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rotation);
+        }
         public override void AddRecipes()
         {
             CreateRecipe().
---
+++ Projectiles/DraedonsArsenal/TaserExplosion.cs
@@ -0,0 +1,50 @@
+﻿using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Buffs.StatDebuffs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Projectiles.Ranged
+{
+    public class TaserExplosion : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Ranged";
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+        public float radius = 100;
+        public override void SetDefaults()
+        {
+            Projectile.width = 200;
+            Projectile.height = 200;
+            Projectile.friendly = true;
+            Projectile.ignoreWater = false;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 2;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            target.AddBuff(BuffID.Electrified, 300);
+
+            if (Projectile.numHits > 0)
+                Projectile.damage = (int)(Projectile.damage * 0.8f);
+            if (Projectile.damage < 1)
+                Projectile.damage = 1;
+        }
+
+        public override void AI()
+        {
+            if (Projectile.ai[0] == 0)
+                radius = 200;
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            return false;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) => CalamityUtils.CircularHitboxCollision(Projectile.Center, radius, targetHitbox);
+    }
+}
