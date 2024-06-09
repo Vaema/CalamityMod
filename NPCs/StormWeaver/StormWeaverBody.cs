759fecbbcf1c6b0c5569fac8ef4bc9ca3f1fc5b7
mountaindrew10 <marhaj04@gmail.com>
2024-06-09 10:56:28 -0500

    Master Mode HP scaling for Bumblefuck, CV, SW, Signut, Guardians, and Provi.

--- NPCs/StormWeaver/StormWeaverBody.cs
+++ NPCs/StormWeaver/StormWeaverBody.cs
@@ -74,6 +74,9 @@ namespace CalamityMod.NPCs.StormWeaver
                 NPC.scale *= 0.7f;
 
             NPC.Calamity().VulnerableToElectricity = false;
+
+            // Scale HP in Master
+            CalamityGlobalNPC.AdjustMasterModeStatScaling(NPC, true);
         }
 
         public override void SendExtraAI(BinaryWriter writer)
