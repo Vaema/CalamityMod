8b0dfed918f41f70b018f98ec0e5fb9443e7d65e
mountaindrew10 <marhaj04@gmail.com>
2024-06-09 10:42:44 -0500

    Master Mode HP scaling for AS, Brim, Cal Clone, Cryogen, Anahita, and Leviathan.

--- NPCs/CalClone/CalamitasClone.cs
+++ NPCs/CalClone/CalamitasClone.cs
@@ -89,6 +89,9 @@ namespace CalamityMod.NPCs.CalClone
             NPC.Calamity().VulnerableToHeat = false;
             NPC.Calamity().VulnerableToCold = true;
             NPC.Calamity().VulnerableToWater = true;
+
+            // Scale HP in Master
+            CalamityGlobalNPC.AdjustMasterModeStatScaling(NPC, true);
         }
 
         public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
