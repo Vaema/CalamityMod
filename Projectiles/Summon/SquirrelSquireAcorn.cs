209becd5101aa9dfcd3f230c5f4ca61f54c90341
Mr-Memes <sorokinalex405@gmail.com>
2024-07-02 19:08:01 +0200

    Squirrel Squire Start.

--- Projectiles/Summon/SquirrelSquireAcorn.cs
+++ Projectiles/Summon/SquirrelSquireAcorn.cs
@@ -1,37 +1,41 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using static CalamityMod.Items.Weapons.Summon.SquirrelSquireStaff;
 
 namespace CalamityMod.Projectiles.Summon
 {
     public class SquirrelSquireAcorn : ModProjectile, ILocalizedModType
     {
         public new string LocalizationCategory => "Projectiles.Summon";
-        public const float Gravity = 0.5f;
-        public override void SetStaticDefaults()
-        {
-            ProjectileID.Sets.SentryShot[Projectile.type] = true;
-        }
+
+        public override void SetStaticDefaults() => ProjectileID.Sets.SentryShot[Type] = true;
 
         public override void SetDefaults()
         {
-            Projectile.width = 10;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.timeLeft = ProjectileTimeAlive;
+            Projectile.width = 16;
             Projectile.height = 16;
             Projectile.friendly = true;
-            Projectile.penetrate = 2;
-            Projectile.timeLeft = 180;
-            Projectile.MaxUpdates = 2;
-            Projectile.DamageType = DamageClass.Summon;
         }
 
         public override void AI()
         {
-            Projectile.velocity.Y += Gravity;
-            Projectile.rotation += (Projectile.velocity.X > 0f).ToDirectionInt() * 0.3f;
+            Projectile.velocity.Y += ProjectileGravity;
+            Projectile.rotation += MathHelper.ToRadians(Projectile.velocity.X * 3f);
         }
 
         public override void OnKill(int timeLeft)
         {
+            Projectile.ExpandHitboxBy(ProjectileAoERadiusSize * 2);
+            if (Main.myPlayer == Projectile.owner)
+                Projectile.Damage();
+
+            if (Main.dedServ)
+                return;
+
             for (int k = 0; k < 5; k++)
                 Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, DustID.WoodFurniture, Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
         }
