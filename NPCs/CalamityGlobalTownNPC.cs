e3d2046230d2bc8bf988940a83caaf1bfa22d9a6
Ozzatron <spazeglitch@gmail.com>
2024-08-13 12:08:06 -0400

    Add Painter name Bew for donor

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -159,6 +159,7 @@ namespace CalamityMod.NPCs
         private static readonly string[] PainterNames =
         {
             "Picasso", // <@!353316526306361347> (sconicboom -- for the late picassosbean2819)
+            "Bew", // <@!232291351167893505> (dmshi)
         };
         private static readonly string[] PartyGirlNames =
         {
