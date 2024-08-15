ffae5afcc99fd4b1c7302cad73cc982ec0961f57
Ozzatron <spazeglitch@gmail.com>
2024-08-15 16:35:51 -0400

    Remove absurd import from Toxicant Twister

--- Items/Weapons/Rogue/ToxicantTwister.cs
+++ Items/Weapons/Rogue/ToxicantTwister.cs
@@ -5,7 +5,6 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
-using static Terraria.GameContent.Bestiary.IL_BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions;
 
 namespace CalamityMod.Items.Weapons.Rogue
 {
