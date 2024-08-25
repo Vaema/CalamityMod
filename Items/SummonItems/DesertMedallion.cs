3978b26a5f97753d21bb6e5cd4c007597528ee11
Flowaria <wnsghgj1234@naver.com>
2024-08-26 01:42:25 +0900

    Netcode Fix: Boss Summon Item issue fixes (#55)
    * Fixed Netcode Issues on BossSummon Items
    
    * Replaced npc loop to ActiveNPCs iterator

--- Items/SummonItems/DesertMedallion.cs
+++ Items/SummonItems/DesertMedallion.cs
@@ -1,9 +1,7 @@
 ﻿using CalamityMod.Events;
 using CalamityMod.Items.Materials;
 using CalamityMod.NPCs.DesertScourge;
-using CalamityMod.World;
 using Terraria;
-using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -43,12 +41,7 @@ namespace CalamityMod.Items.SummonItems
 
         public override bool? UseItem(Player player)
         {
-            SoundEngine.PlaySound(SummonSound, player.Center);
-            if (Main.netMode != NetmodeID.MultiplayerClient)
-                NPC.SpawnOnPlayer(player.whoAmI, ModContent.NPCType<DesertScourgeHead>());
-            else
-                NetMessage.SendData(MessageID.SpawnBossUseLicenseStartEvent, -1, -1, null, player.whoAmI, ModContent.NPCType<DesertScourgeHead>());
-
+            CalamityUtils.SpawnBossUsingItem<DesertScourgeHead>(player, SummonSound);
             return true;
         }
 
