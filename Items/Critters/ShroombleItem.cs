bd9a582e52cc5945f1aa218bb7cf2f1f6e0b9d82
Altixal <73398287+Altixal@users.noreply.github.com>
2024-09-19 02:48:36 +0700

    shroomble

--- Items/Critters/ShroombleItem.cs
+++ Items/Critters/ShroombleItem.cs
@@ -15,20 +15,8 @@ namespace CalamityMod.Items.Critters
 
         public override void SetDefaults()
         {
-            Item.width = 26;
-            Item.height = 24;
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.autoReuse = true;
-            Item.useTurn = true;
-            Item.useAnimation = 15;
-            Item.useTime = 10;
-            Item.maxStack = 9999;
-            Item.consumable = true;
-            Item.noUseGraphic = true;
-            Item.makeNPC = (short)ModContent.NPCType<Shroomble>();
-
+            Item.DefaultToCapturedCritter(ModContent.NPCType<Shroomble>());
             Item.value = Item.sellPrice(silver: 1);
-            Item.rare = ItemRarityID.White;
         }
     }
 }
