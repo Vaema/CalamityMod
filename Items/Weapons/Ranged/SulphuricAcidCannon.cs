34a3b90807323c61d614209371a5918cfcc03546
mochi-angel <snowyqueenofducks@gmail.com>
2024-09-19 02:17:27 -0700

    Missing firing effects and cluster behavior

--- Items/Weapons/Ranged/SulphuricAcidCannon.cs
+++ Items/Weapons/Ranged/SulphuricAcidCannon.cs
@@ -1,8 +1,11 @@
 ﻿using CalamityMod.Projectiles.Ranged;
 using CalamityMod.Rarities;
 using Microsoft.Xna.Framework;
+using Terraria.DataStructures;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using System;
 
 namespace CalamityMod.Items.Weapons.Ranged
 {
@@ -18,21 +21,64 @@ namespace CalamityMod.Items.Weapons.Ranged
         {
             Item.width = 90;
             Item.height = 30;
-            Item.damage = 144;
+            Item.damage = 550;
             Item.DamageType = DamageClass.Ranged;
-            Item.useTime = 18;
-            Item.useAnimation = 18;
+            Item.useTime = 35;
+            Item.useAnimation = 35;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.noMelee = true;
             Item.knockBack = 6f;
             Item.autoReuse = true;
             Item.value = CalamityGlobalItem.RarityPureGreenBuyPrice;
-            Item.UseSound = SoundID.Item95;
+            Item.UseSound = SoundID.Item73;
+            Item.useAmmo = AmmoID.Rocket;
             Item.shoot = ModContent.ProjectileType<SulphuricBlast>();
             Item.shootSpeed = 16f;
             Item.rare = ModContent.RarityType<PureGreen>();
         }
 
         public override Vector2? HoldoutOffset() => Vector2.UnitX * -15f;
+
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            //Slight randomization
+            float SpeedX = velocity.X + (float)Main.rand.Next(-40, 41) * 0.05f;
+            float SpeedY = velocity.Y + (float)Main.rand.Next(-40, 41) * 0.05f;
+            Projectile.NewProjectile(source, position.X, position.Y, SpeedX, SpeedY, Item.shoot, damage, knockback, player.whoAmI);
+            return false;
+        }
+
+        public override void UseStyle(Player player, Rectangle heldItemFrame)
+        {
+            player.ChangeDir(Math.Sign((player.Calamity().mouseWorld - player.Center).X));
+            float itemRotation = player.compositeFrontArm.rotation + MathHelper.PiOver2 * player.gravDir;
+
+            float pullback = 7f;
+
+            float animProgress = 0.5f - player.itemTime / (float)player.itemTimeMax;
+            float rotation = (player.Center - player.Calamity().mouseWorld).ToRotation() * player.gravDir + MathHelper.PiOver2;
+            if (animProgress < 0.4f)
+                pullback -= (2.75f) * (float)Math.Pow((0.6f - animProgress) / 0.6f, 2);
+
+            Vector2 itemPosition = player.MountedCenter + itemRotation.ToRotationVector2() * pullback;
+            Vector2 itemSize = new Vector2(52, 28);
+            Vector2 itemOrigin = new Vector2(-24, 4);
+
+            CalamityUtils.CleanHoldStyle(player, itemRotation, itemPosition, itemSize, itemOrigin);
+
+            base.UseStyle(player, heldItemFrame);
+        }
+
+        public override void UseItemFrame(Player player)
+        {
+            player.ChangeDir(Math.Sign((player.Calamity().mouseWorld - player.Center).X));
+
+            float animProgress = 0.5f - player.itemTime / (float)player.itemTimeMax;
+            float rotation = (player.Center - player.Calamity().mouseWorld).ToRotation() * player.gravDir + MathHelper.PiOver2;
+            if (animProgress < 0.4f)
+                rotation += (player.altFunctionUse == 2 ? -0.15f : 0) * (float)Math.Pow((0.6f - animProgress) / 0.6f, 2) * player.direction;
+
+            player.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, rotation);
+        }
     }
 }
--- Projectiles/Ranged/SulphuricBlast.cs
+++ Projectiles/Ranged/SulphuricBlast.cs
@@ -1,8 +1,14 @@
 ﻿using CalamityMod.Buffs.DamageOverTime;
+using CalamityMod.Graphics.Primitives;
 using CalamityMod.Particles;
+using CalamityMod.Projectiles.Magic;
+using CalamityMod.Projectiles.Rogue;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -11,10 +17,19 @@ namespace CalamityMod.Projectiles.Ranged
     public class SulphuricBlast : ModProjectile, ILocalizedModType
     {
         public new string LocalizationCategory => "Projectiles.Ranged";
-        public const int TotalSecondsToStick = 8;
+        public NPC chosenTarget;
+        public bool stuckInTarget = false;
+        public bool stuckInGround = false;
+        public int stuckTimer = 200;
+        public bool canDamage = true;
+        public bool canStick = true;
+        public Vector2 vibrate = Vector2.Zero;
+        public Vector2 placementCenter;
+        float placementDistance;
+        public Vector2 storedVelocity;
+        Vector2 placementVelocity;
         public override void SetStaticDefaults()
         {
-            Main.projFrames[Projectile.type] = 20;
             ProjectileID.Sets.CultistIsResistantTo[Type] = true;
         }
 
@@ -29,48 +44,130 @@ namespace CalamityMod.Projectiles.Ranged
             Projectile.penetrate = -1;
             Projectile.alpha = 255;
             Projectile.MaxUpdates = 3;
+            Projectile.timeLeft = 600;
             Projectile.usesLocalNPCImmunity = true;
             Projectile.localNPCHitCooldown = Projectile.MaxUpdates * 11;
         }
 
         public override void AI()
         {
-            if (Projectile.FinalExtraUpdate())
-                Projectile.frameCounter++;
-            Projectile.frame = Projectile.frameCounter / 4 % Main.projFrames[Projectile.type];
-
-            Projectile.StickyProjAI(8);
-            Projectile.Opacity = Utils.GetLerpValue(CalamityUtils.SecondsToFrames(TotalSecondsToStick), CalamityUtils.SecondsToFrames(TotalSecondsToStick * 0.5f), Projectile.localAI[0], true);
-
-            // Emit sulpuric gas.
-            if (Main.netMode != NetmodeID.Server && Projectile.FinalExtraUpdate() && Projectile.velocity.Length() > 3f)
-            {
-                Color color = new Color(136, 211, 113, 127);
-                Color fadeColor = new Color(165, 165, 86);
-                Vector2 gasSpawnPosition = Projectile.Center + Main.rand.NextVector2Circular(8f, 8f);
-                Vector2 gasVelocity = Projectile.velocity * 1.2f + Projectile.velocity.RotatedBy(0.75f) * 0.3f;
-                gasVelocity *= Main.rand.NextFloat(0.24f, 0.6f);
-
-                Particle gas = new MediumMistParticle(gasSpawnPosition, gasVelocity, color, fadeColor, Main.rand.NextFloat(0.5f, 1f), 205 - Main.rand.Next(50), 0.02f);
-                GeneralParticleHandler.SpawnParticle(gas);
-            }
-
+            Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+            float power = 4;
+            vibrate = Main.rand.NextVector2Circular(power, power);
             // Home in on enemies if not sticking to anything.
-            if (Projectile.ai[0] != 1f)
+            if (!stuckInTarget)
             {
                 CalamityUtils.HomeInOnNPC(Projectile, true, 720f, 16f, Projectile.MaxUpdates * 20f);
-                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
+                // Trailing effects, only applied when homing
+                if (Main.netMode != NetmodeID.Server && Projectile.FinalExtraUpdate() && Projectile.velocity.Length() > 3f)
+                {
+                    Color color = new Color(136, 211, 113, 127);
+                    Color fadeColor = new Color(165, 165, 86);
+                    Vector2 gasSpawnPosition = Projectile.Center + Main.rand.NextVector2Circular(8f, 8f);
+                    Vector2 gasVelocity = Projectile.velocity * 1.2f + Projectile.velocity.RotatedBy(0.75f) * 0.3f;
+                    gasVelocity *= Main.rand.NextFloat(0.24f, 0.6f);
+
+                    Particle gas = new MediumMistParticle(gasSpawnPosition, gasVelocity, color, fadeColor, Main.rand.NextFloat(0.5f, 1f), 205 - Main.rand.Next(50), 0.02f);
+                    GeneralParticleHandler.SpawnParticle(gas);
+                    if (Projectile.timeLeft <= 590)
+                    {
+                        for (int i = 0; i < 2; i++)
+                        {
+                            Particle spark = new GlowSparkParticle(Projectile.Center - Projectile.velocity.SafeNormalize(Vector2.UnitX) * 10, -Projectile.velocity, false, 8, 0.13f * (i == 0 ? 0.2f : 0.5f), Color.Lerp(Color.SeaGreen, Color.PaleGreen, 0.25f) * 0.5f, new Vector2(0.3f, 1f), false, false, 0.8f);
+                            GeneralParticleHandler.SpawnParticle(spark);
+                        }
+                        for (int i = 0; i < 2; i++)
+                        {
+                            Color bubbleColor = Main.rand.NextBool() ? Color.SeaGreen : Color.YellowGreen;
+                            Vector2 bubbleSpawnPos = Projectile.Center + Main.rand.NextVector2Circular(50, 50);
+                            Vector2 bubbleVelocity = -Projectile.velocity * Main.rand.NextFloat(0.2f, 0.8f);
+                            Particle bubble = new DirectionalPulseRing(bubbleSpawnPos, bubbleVelocity, bubbleColor, new Vector2(0.8f, 1), 0, 0.1f, 0f, 75);
+                            GeneralParticleHandler.SpawnParticle(bubble);
+                        }
+                    }                                   
+                    //Firing Effects
+                    if (Projectile.timeLeft <= 595)
+                    {
+                        Particle pulse = new DirectionalPulseRing(Projectile.Center, Projectile.velocity / 100f, Color.Aqua, new Vector2(1f, 2.5f), Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2, 0.2f, 0.03f, 20);
+                        GeneralParticleHandler.SpawnParticle(pulse);
+                        Particle pulse2 = new DirectionalPulseRing(Projectile.Center, Projectile.velocity / 90f, Color.DodgerBlue, new Vector2(1f, 2.5f), Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2, 0.1f, 0.025f, 35);
+                        GeneralParticleHandler.SpawnParticle(pulse2);
+                    }
+                }
+            }
+            else if (stuckInTarget)
+            {
+                placementCenter = chosenTarget.Center + placementVelocity * placementDistance + storedVelocity;
+
+                Projectile.Center = placementCenter;
+                Projectile.rotation = (storedVelocity).SafeNormalize(Vector2.UnitX).ToRotation() + MathHelper.PiOver2;
+                stuckTimer--;
+                if (chosenTarget.life <= 0 || chosenTarget == null)
+                    stuckTimer = 0;
+                if (stuckTimer == 0)
+                {
+                    Projectile.Kill();
+                }
+                if (stuckTimer == 40)
+                {
+                    SoundStyle Primed = new("CalamityMod/Sounds/Custom/PlagueSounds/PBGAttackSwitchShort");
+                    SoundEngine.PlaySound(Primed with { Volume = 0.5f }, Projectile.Center);
+                }
+            }
+            Projectile.Opacity = 1f;
+        }
+        public override bool? CanDamage() => canDamage ? null : false;
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            if (target == chosenTarget)
+                target.AddBuff(ModContent.BuffType<SulphuricPoisoning>(), 180);
+            if (!stuckInTarget && canStick)
+            {
+                canDamage = false;
+                Projectile.rotation = (Projectile.velocity).SafeNormalize(Vector2.UnitX).ToRotation() + MathHelper.PiOver2;
+                placementDistance = -Vector2.Distance(target.Center, Projectile.Center);
+                placementVelocity = (target.Center - Projectile.Center).SafeNormalize(Vector2.UnitX);
+                placementCenter = placementVelocity * (placementDistance * 0.01f);
+                chosenTarget = target;
+                stuckInTarget = true;
+                storedVelocity = Projectile.velocity.SafeNormalize(Vector2.UnitX) * 8;
+                Projectile.velocity = Vector2.Zero;
             }
         }
 
-        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone) => target.AddBuff(ModContent.BuffType<SulphuricPoisoning>(), 180);
-
         public override void OnHitPlayer(Player target, Player.HurtInfo info) => target.AddBuff(ModContent.BuffType<SulphuricPoisoning>(), 180);
 
-        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers) => Projectile.ModifyHitNPCSticky(2);
+        public override void OnKill(int timeLeft)
+        {
+            SoundStyle fire = new("CalamityMod/Sounds/Custom/PlagueSounds/PlagueBoom", 4);
+            SoundEngine.PlaySound(fire with { Volume = 0.9f, Pitch = -0.3f }, Projectile.Center);
+            SoundEngine.PlaySound(SoundID.Item107, Projectile.Center);
+            if (Projectile.owner == Main.myPlayer)
+            {
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<SulphuricAcidCannonExplosion>(), (int)(Projectile.damage * 2), Projectile.knockBack, Projectile.owner, ai1: 160f);
+            }
+            // Circular spread of clouds and bubbles for impact
+            for (int i = 0; i < 35; i++)
+            {
+                Vector2 smokeVel = Main.rand.NextVector2Unit() * Main.rand.NextVector2Circular(32f, 32f);
+                Color smokeColor = Main.rand.NextBool() ? Color.SeaGreen : Color.PaleGreen;
+                Particle smoke = new MediumMistParticle(Projectile.Center, smokeVel, smokeColor, Color.Black, Main.rand.NextFloat(1.4f, 4f), 200 - Main.rand.Next(60), 0.08f);
+                GeneralParticleHandler.SpawnParticle(smoke);
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                Vector2 bubbleVel = Main.rand.NextVector2Circular(26f, 26f);
+                Color bubbleColor = Main.rand.NextBool() ? Color.OliveDrab : Color.SeaGreen;
+                DirectionalPulseRing pulse = new DirectionalPulseRing(Projectile.Center, bubbleVel, bubbleColor, new Vector2(0.8f, 1), 0, 0.21f, 0f, 50);
+                GeneralParticleHandler.SpawnParticle(pulse);
+            }          
+            //Explosion effect
+            Particle blastRing = new CustomPulse(Projectile.Center, Vector2.Zero, Color.SeaGreen, "CalamityMod/Particles/FlameExplosion", Vector2.One, Main.rand.NextFloat(-10, 10), 0f, 0.2f, 20, true, 1f);
+            GeneralParticleHandler.SpawnParticle(blastRing);
+        }
 
         public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
-        {
+        {     
             if (targetHitbox.Width > 8 && targetHitbox.Height > 8)
             {
                 targetHitbox.Inflate(-targetHitbox.Width / 8, -targetHitbox.Height / 8);
@@ -83,7 +180,7 @@ namespace CalamityMod.Projectiles.Ranged
             Texture2D texture2D13 = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value;
             int projFrame = Terraria.GameContent.TextureAssets.Projectile[Projectile.type].Value.Height / Main.projFrames[Projectile.type];
             int y6 = projFrame * Projectile.frame;
-            Main.spriteBatch.Draw(texture2D13, Projectile.Center - Main.screenPosition + new Vector2(0f, Projectile.gfxOffY), new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, y6, texture2D13.Width, projFrame)), Projectile.GetAlpha(lightColor), Projectile.rotation, new Vector2((float)texture2D13.Width / 2f, (float)projFrame / 2f), Projectile.scale, SpriteEffects.None, 0);
+            Main.spriteBatch.Draw(texture2D13, Projectile.Center - Main.screenPosition + vibrate, new Microsoft.Xna.Framework.Rectangle?(new Rectangle(0, y6, texture2D13.Width, projFrame)), Projectile.GetAlpha(lightColor), Projectile.rotation, new Vector2((float)texture2D13.Width / 2f, (float)projFrame / 2f), Projectile.scale, SpriteEffects.None, 0);
             return false;
         }
     }
