3ffa2230ca011be42f536888d9ae82617addc76c
Flowaria <wnsghgj1234@naver.com>
2024-09-30 07:34:14 +0900

    the fuck is this

--- Systems/TileMerging/DataStructures.cs
+++ Systems/TileMerging/DataStructures.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.DataStructures;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CalamityMod.Systems
 {
