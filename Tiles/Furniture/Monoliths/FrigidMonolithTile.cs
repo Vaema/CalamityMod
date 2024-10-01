db5a8ac9489d05037b1a1177921e4f9015b98b0e
Flowaria <wnsghgj1234@naver.com>
2024-10-01 17:27:21 +0900

    Set Origin of FrigidMonolith to Ground

--- Tiles/Furniture/Monoliths/FrigidMonolithTile.cs
+++ Tiles/Furniture/Monoliths/FrigidMonolithTile.cs
@@ -29,7 +29,7 @@ namespace CalamityMod.Tiles.Furniture.Monoliths
             RegisterItemDrop(ModContent.ItemType<FrigidMonolith>());
             Main.tileFrameImportant[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style3x4);
-            TileObjectData.newTile.Origin = new Point16(0, 2);
+            TileObjectData.newTile.Origin = new Point16(0, 3);
             TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 18 };
             TileObjectData.newTile.LavaDeath = false;
             TileObjectData.newTile.UsesCustomCanPlace = true;
