a22989177b7d5d28fa943bfce3b0fb1d1806147f
Memes <114233643+Mr-Memes@users.noreply.github.com>
2024-10-05 00:35:10 +0200

    Made BaseSummonBuff abstract class.

--- Buffs/Summon/FrostbatBuff.cs
+++ Buffs/Summon/FrostbatBuff.cs
@@ -1,35 +1,12 @@
-﻿using CalamityMod.CalPlayer;
 using CalamityMod.Projectiles.Summon;
-using Terraria;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Buffs.Summon
 {
-    public class FrostbatBuff : ModBuff
+    public class FrostbatBuff : BaseSummonBuff
     {
-        public override void SetStaticDefaults()
-        {
-            Main.buffNoTimeDisplay[Type] = true;
-            Main.buffNoSave[Type] = true;
-        }
+        protected override int MinionProjectileType => ModContent.ProjectileType<FrostbatMinion>();
 
-        public override void Update(Player player, ref int buffIndex)
-        {
-            CalamityPlayer modPlayer = player.Calamity();
-            if (player.ownedProjectileCounts[ModContent.ProjectileType<FrostbatMinion>()] > 0)
-            {
-                modPlayer.FrostbatBool = true;
-            }
-
-            if (!modPlayer.FrostbatBool)
-            {
-                player.DelBuff(buffIndex);
-                buffIndex--;
-            }
-            else
-            {
-                player.buffTime[buffIndex] = 18000;
-            }
-        }
+        protected override ref bool MinionBool => ref BuffModdedOwner.FrostbatBool;
     }
 }
--- Buffs/Summon/SepulcherMinionBuff.cs
+++ Buffs/Summon/SepulcherMinionBuff.cs
@@ -1,33 +1,12 @@
-﻿using CalamityMod.CalPlayer;
 using CalamityMod.Projectiles.Summon;
-using Terraria;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Buffs.Summon
 {
-    public class SepulcherMinionBuff : ModBuff
+    public class SepulcherMinionBuff : BaseSummonBuff
     {
-        public override void SetStaticDefaults()
-        {
-            Main.buffNoTimeDisplay[Type] = true;
-            Main.buffNoSave[Type] = true;
-        }
+        protected override int MinionProjectileType => ModContent.ProjectileType<SepulcherMinion>();
 
-        public override void Update(Player player, ref int buffIndex)
-        {
-            CalamityPlayer modPlayer = player.Calamity();
-            if (player.ownedProjectileCounts[ModContent.ProjectileType<SepulcherMinion>()] > 0)
-                modPlayer.sepulcher = true;
-
-            if (!modPlayer.sepulcher)
-            {
-                player.DelBuff(buffIndex);
-                buffIndex--;
-            }
-            else
-            {
-                player.buffTime[buffIndex] = 18000;
-            }
-        }
+        protected override ref bool MinionBool => ref BuffModdedOwner.sepulcher;
     }
 }
