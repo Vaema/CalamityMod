06858467efac259684dabc3e9aa6b33672da70b7
GinYuH <72181395+GinYuH@users.noreply.github.com>
2024-10-18 19:42:37 -0700

    Added Repair Unit Cage

---
+++ Tiles/Furniture/RepairUnitCageTile.cs
@@ -0,0 +1,56 @@
+﻿using CalamityMod.Items.Placeables.Furniture;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityMod.Tiles.Furniture
+{
+    public class RepairUnitCageTile : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            Main.tileSolidTop[Type] = true;
+            Main.tileTable[Type] = true;
+            TileID.Sets.CritterCageLidStyle[Type] = TileID.Sets.CritterCageLidStyle[TileID.BunnyCage]; // Big, gray lid
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
+            TileObjectData.addTile(Type);
+            AnimationFrameHeight = 54;
+            AddMapEntry(new Color(122, 217, 232), CalamityUtils.GetItemName<RepairUnitCage>());
+        }
+
+        public override bool CreateDust(int i, int j, ref int type)
+        {
+            Dust.NewDust(new Vector2(i, j) * 16f, 16, 16, DustID.Glass, 0f, 0f, 0, new Color(), 1f);
+            return false;
+        }
+
+        public override void NumDust(int i, int j, bool fail, ref int num)
+        {
+            num = fail ? 1 : 3;
+        }
+
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) => offsetY = 2;
+
+        public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            int frameAmt = 10;
+            int timeNeeded = 10;
+            frameCounter++;
+            if (frameCounter >= timeNeeded)
+            {
+                frame++;
+                frameCounter = 0;
+            }
+            if (frame >= frameAmt)
+            {
+                frame = 0;
+            }
+        }
+    }
+}
