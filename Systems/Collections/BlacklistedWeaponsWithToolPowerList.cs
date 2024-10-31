4f9a561c61416062dd4149ac3f564631300d7f9f
Flowaria <wnsghgj1234@naver.com>
2024-10-31 18:17:25 +0900

    Code Cleanup

--- Systems/Collections/BlacklistedWeaponsWithToolPowerList.cs
+++ Systems/Collections/BlacklistedWeaponsWithToolPowerList.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using CalamityMod.Items.Tools;
 using CalamityMod.Items.Weapons.DraedonsArsenal;
 using CalamityMod.Items.Weapons.Melee;
