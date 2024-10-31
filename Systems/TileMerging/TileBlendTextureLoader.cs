08e9e5c4d98a052426881532a4d154debbc62e13
Flowaria <wnsghgj1234@naver.com>
2024-10-31 13:10:35 +0900

    Changed AllTextures Logic

--- Systems/TileMerging/TileBlendTextureLoader.cs
+++ Systems/TileMerging/TileBlendTextureLoader.cs
@@ -18,7 +18,7 @@ namespace CalamityMod.Systems
         internal const int MaxCount = 255;
 
         internal static TileBlendTexture[] Registry { get; private set; }
-        internal static IEnumerable<TileBlendTexture> AllTextures => Registry.Take(Count);
+        internal static IEnumerable<TileBlendTexture> AllTextures => Registry.Where(tex => tex is not null);
         internal static int Count => _UniqueSlot - StartingIndex;
 
         private static int _UniqueSlot = StartingIndex;
