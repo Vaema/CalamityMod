efe21ddfbfb5bc24d9408aa610de2d9b7b4497e7
Xyk <MetroidWhisperer@gmail.com>
2024-11-14 12:05:23 -0800

    Aetherflux Glowup and Nano Purge Tierswap
    Plasma Rifle and Nano Purge swap Places
    Aetherflux cannon gets some new visuals and balancing

--- Items/Weapons/Magic/AetherfluxCannon.cs
+++ Items/Weapons/Magic/AetherfluxCannon.cs
@@ -15,12 +15,14 @@ namespace CalamityMod.Items.Weapons.Magic
     {
         public new string LocalizationCategory => "Items.Weapons.Magic";
         public const int UseTime = 36;
+        public static Color mainColor = Color.Goldenrod;
+        public static Color accentColor = Color.LightGreen;
 
         public override void SetDefaults()
         {
             Item.width = 94;
             Item.height = 54;
-            Item.damage = 285;
+            Item.damage = 355;
             Item.DamageType = DamageClass.Magic;
             Item.mana = 7;
             Item.useAnimation = Item.useTime = UseTime;
