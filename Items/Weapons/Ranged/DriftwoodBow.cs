d4c15b99a28e00eec2eb4625ee1090cf7e513a29
Memes <114233643+Mr-Memes@users.noreply.github.com>
2024-11-23 16:00:01 +0100

    MP-fixed Driftwood Bow & Sword.

--- Items/Weapons/Ranged/DriftwoodBow.cs
+++ Items/Weapons/Ranged/DriftwoodBow.cs
@@ -1,7 +1,6 @@
 ﻿using CalamityMod.Items.Placeables.SunkenSea;
 using Microsoft.Xna.Framework;
 using Terraria;
-using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -10,6 +9,7 @@ namespace CalamityMod.Items.Weapons.Ranged
     public class DriftwoodBow : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Ranged";
+
         public override void SetDefaults()
         {
             Item.damage = 10;
@@ -31,38 +31,35 @@ namespace CalamityMod.Items.Weapons.Ranged
             Item.Calamity().canFirePointBlankShots = true;
         }
 
-        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
-            bool surface = player.Center.Y < Main.worldSurface * 16.0;
-            bool GetEffects = ((Main.raining && surface) || player.dripping || (player.wet && !player.lavaWet && !player.honeyWet));
-            if (GetEffects)
+            if (IsPlayerInContactWithWater(player))
             {
+                velocity *= 1.3f;
+                knockback += 1f;
+
                 for (int i = 0; i <= 18; i++)
                 {
-                    Dust dust = Dust.NewDustPerfect(position + velocity * 3, 160, velocity.RotatedByRandom(MathHelper.ToRadians(19f)) * Main.rand.NextFloat(0.8f, 3.8f), 0, default, Main.rand.NextFloat(1.2f, 1.6f));
+                    Dust dust = Dust.NewDustPerfect(position + velocity * 3f, 160, velocity.RotatedByRandom(MathHelper.ToRadians(19f)) * Main.rand.NextFloat(0.8f, 3.8f), Scale: Main.rand.NextFloat(1.2f, 1.6f));
                     dust.noGravity = true;
                 }
-                Item.useTime = 23;
-                Item.useAnimation = 23;
-                Item.knockBack = 1f;
-                Item.shootSpeed = 8.6f;
             }
-            else
-            {
-                Item.useTime = 28;
-                Item.useAnimation = 28;
-                Item.knockBack = 0f;
-                Item.shootSpeed = 6.6f;
-            }
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
-            return false;
         }
+
+        public override float UseSpeedMultiplier(Player player) => IsPlayerInContactWithWater(player) ? 1.2f : 1f;
+
         public override void AddRecipes()
         {
             CreateRecipe().
                 AddIngredient<Driftwood>(10).
                 AddTile(TileID.WorkBenches).
                 Register();
+        }
+
+        private static bool IsPlayerInContactWithWater(Player player)
+        {
+            bool surface = player.Center.Y < Main.worldSurface * 16.0;
+            return (Main.raining && surface) || player.dripping || (player.wet && !player.lavaWet && !player.honeyWet);
         }
     }
 }
