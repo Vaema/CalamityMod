1a8decf9d853ebc529a6c19f84661408a14109df
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-11-24 23:22:01 -0500

    Auralis actually summons an "aurora" now

--- CalPlayer/CalamityPlayerMiscEffects.cs
+++ CalPlayer/CalamityPlayerMiscEffects.cs
@@ -2792,6 +2792,14 @@ namespace CalamityMod.CalPlayer
                     auralisAuroraCounter = 0;
                     auralisAuroraCooldown = CalamityUtils.SecondsToFrames(30f);
                 }
+                else if (auralisAuroraCounter > chargeDuration)
+                {
+                    for (int i = -1; i <= 1; i += 2)
+                    {
+                        GlowSquareParticle fancySquares = new(Player.Center, Vector2.Zero, false, 2, 6.25f, new Color(92, 89, 251), true, MathHelper.ToRadians(auralisAuroraCounter * 2f * i));
+                        GeneralParticleHandler.SpawnParticle(fancySquares);
+                    }
+                }
 
                 if (auralisAuroraCounter > 0 && auralisAuroraCounter < chargeDuration && !usingScope)
                     auralisAuroraCounter--;
