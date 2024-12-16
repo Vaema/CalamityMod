4b3149dd75bc42537fb4580cfb4464de92d6f83d
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2024-12-16 14:59:38 -0500

    Reworked Meteor Fist

--- Projectiles/Rogue/TalonSmallProj.cs
+++ Projectiles/Rogue/TalonSmallProj.cs
@@ -10,9 +10,6 @@ namespace CalamityMod.Projectiles.Rogue
     public class TalonSmallProj : ModProjectile, ILocalizedModType
     {
         public new string LocalizationCategory => "Projectiles.Rogue";
-        private static float SineFrequency = 0.05f;
-        private static float SineAmplitude = 0.008f;
-        private static float RotationIncrement = 0.26f;
 
         public override void SetDefaults()
         {
