6b94394a7b93e246df0180e8a410b3ecadbf9eb9
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-01-11 19:02:26 -0500

    Cranium Smasher stuff

--- Items/Weapons/Rogue/CraniumSmasher.cs
+++ Items/Weapons/Rogue/CraniumSmasher.cs
@@ -9,27 +9,27 @@ namespace CalamityMod.Items.Weapons.Rogue
 {
     public class CraniumSmasher : RogueWeapon
     {
+        private bool throwExplosive = false;
         public override void SetDefaults()
         {
             Item.width = 50;
             Item.height = 50;
-            Item.damage = 120;
+            Item.damage = 140;
+            Item.DamageType = RogueDamageClass.Instance;
+            Item.useAnimation = Item.useTime = 20;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 4f;
             Item.noMelee = true;
             Item.noUseGraphic = true;
             Item.autoReuse = true;
-            Item.useAnimation = 15;
-            Item.useStyle = ItemUseStyleID.Swing;
-            Item.useTime = 15;
-            Item.knockBack = 4f;
             Item.UseSound = SoundID.Item1;
             Item.value = CalamityGlobalItem.RarityYellowBuyPrice;
             Item.rare = ItemRarityID.Yellow;
             Item.shoot = ModContent.ProjectileType<CraniumSmasherProj>();
             Item.shootSpeed = 20f;
-            Item.DamageType = RogueDamageClass.Instance;
         }
 
-        public override float StealthDamageMultiplier => 1.75f;
+        public override float StealthDamageMultiplier => 1f;
 
         public override void ModifyStatsExtra(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
         {
@@ -37,14 +37,10 @@ namespace CalamityMod.Items.Weapons.Rogue
             {
                 type = ModContent.ProjectileType<CraniumSmasherStealth>();
             }
-            else if (Main.rand.NextBool(5))
-            {
-                damage = (int)(damage * 1.25f);
-                type = ModContent.ProjectileType<CraniumSmasherExplosive>();
-            }
             else
             {
-                type = ModContent.ProjectileType<CraniumSmasherProj>();
+                type = throwExplosive ? ModContent.ProjectileType<CraniumSmasherExplosive>() : ModContent.ProjectileType<CraniumSmasherProj>();
+                throwExplosive = !throwExplosive;
             }
         }
 
