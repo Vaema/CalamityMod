6931a703b297b117dc5e72140115360e9a8d8b92
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-02-06 17:25:17 -0500

    Changed Pyre Mantle's map color

--- Tiles/Abyss/PyreMantle.cs
+++ Tiles/Abyss/PyreMantle.cs
@@ -28,7 +28,7 @@ namespace CalamityMod.Tiles.Abyss
             HitSound = MineSound;
             MineResist = 10f;
             MinPick = 180;
-            AddMapEntry(new Color(19, 20, 32));
+            AddMapEntry(new Color(43, 40, 40));
 
             this.RegisterUniversalMerge(TileID.Dirt, "CalamityMod/Tiles/Merges/DirtMerge");
             this.RegisterUniversalMerge(TileID.Stone, "CalamityMod/Tiles/Merges/StoneMerge");
