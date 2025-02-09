94c0ca587a8dda87bfe64d9e9a07c5db21cee7fb
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-02-08 21:07:04 -0800

    added Polyperil

---
+++ NPCs/SunkenSea/Polyperil.cs
@@ -0,0 +1,167 @@
+﻿using CalamityMod.Enums;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.NPCs.SunkenSea
+{
+    public class Polyperil : SunkenSeaNPC
+    {
+        public ref float Color => ref NPC.ai[1];
+
+        public ref float Timer => ref NPC.ai[0];
+
+        protected override SunkenSeaBiomeFlags BiomeDesignation => SunkenSeaBiomeFlags.RadiantReefs | SunkenSeaBiomeFlags.PolypForest;
+
+        protected override float SpawningChance => 0.2f;
+
+        public static Asset<Texture2D> blueTexture = null;
+
+        public static Asset<Texture2D> greenTexture = null;
+
+        public static Asset<Texture2D> pinkTexture = null;
+
+        public static Asset<Texture2D> radiantTexture = null;
+
+        protected override List<int> HuntNPCs => new List<int>()
+        {
+            ModContent.NPCType<SeaMinnow>(),
+            ModContent.NPCType<AlphaSeaMinnow>(),
+            ModContent.NPCType<SandProwler>(),
+            ModContent.NPCType<SandProwlerNested>(),
+            ModContent.NPCType<KelpieSeadragon>()
+        };
+
+        // It cannot move lol
+        protected override List<int> AvoidNPCs => new List<int>();
+
+        public override void Load()
+        {
+            pinkTexture = ModContent.Request<Texture2D>(Texture + "Pink");
+            blueTexture = ModContent.Request<Texture2D>(Texture + "Blue");
+            greenTexture = ModContent.Request<Texture2D>(Texture + "Green");
+            radiantTexture = ModContent.Request<Texture2D>(Texture + "Radiant");
+        }
+
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            NPC.damage = 20;
+            NPC.lifeMax = 200;
+            NPC.defense = 10;
+            NPC.knockBackResist = 0f;
+            NPC.chaseable = false;
+
+            NPC.aiStyle = -1;
+            AIType = -1;
+            NPC.width = 32;
+            NPC.height = 36;
+
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.value = Item.buyPrice(0, 0, 5, 0);
+            // Banner = NPC.type;
+            // BannerItem = ModContent.ItemType<PolyperilBanner>();
+
+            NPC.Calamity().VulnerableToHeat = false;
+            NPC.Calamity().VulnerableToSickness = false;
+            NPC.Calamity().VulnerableToElectricity = true;
+            NPC.Calamity().VulnerableToWater = false;
+        }
+
+        protected override void BehaviorOnSpawn()
+        {
+            // Pick a random color
+            Color = Main.rand.Next(0, 3);
+            // 1 in 30 chance to be Radiant
+            if (Main.rand.NextBool(30))
+                Color = 3;
+
+            // Spawn tentacles
+            int dist = 80;
+            int tentMin = 3;
+            int tentMax = 8;
+            for (int i = 0; i < Main.rand.Next(tentMin, tentMax + 1); i++)
+            {
+                if (Main.netMode != NetmodeID.MultiplayerClient)
+                {
+                    Vector2 pos = NPC.Center + new Vector2(Main.rand.Next(-dist, dist), Main.rand.Next(-dist, dist));
+                    NPC tent = NPC.NewNPCDirect(NPC.GetSource_FromThis(), NPC.Center, ModContent.NPCType<PolyperilTentacle>(), ai0: NPC.whoAmI);
+                    tent.ModNPC<PolyperilTentacle>().anchor = pos;
+                }
+            }
+        }
+
+        protected override void CreatureAI()
+        {
+            Timer++;
+            NPC.velocity.Y += 10;
+
+            // While out of water, slowly lose health (suffocating)
+            if (!NPC.wet)
+            {
+                if (Timer % 60 == 0)
+                {
+                    NPC.HitEffect(0, 5);
+                    NPC.life -= 5;
+                    if (NPC.life <= 0)
+                        NPC.life = 1;
+                }
+            }
+        }
+
+        public override void HitEffect(NPC.HitInfo hit)
+        {
+            for (int k = 0; k < 5; k++)
+            {
+                Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.Coralstone, hit.HitDirection, -1f, 0, default, 1f);
+            }
+            if (NPC.life <= 0)
+            {
+                for (int k = 0; k < 25; k++)
+                {
+                    Dust.NewDust(NPC.position, NPC.width, NPC.height, DustID.Coralstone, hit.HitDirection, -1f, 0, default, 1f);
+                }
+            }
+        }
+
+        public override bool CanBeHitByNPC(NPC attacker)
+        {
+            if (attacker.type == ModContent.NPCType<Polyperil>() || attacker.type == ModContent.NPCType<PolyperilTentacle>())
+                return false;
+
+            return true;
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            Texture2D tex = Color switch
+            {
+                1 => blueTexture.Value,
+                2 => greenTexture.Value,
+                3 => radiantTexture.Value,
+                _ => pinkTexture.Value
+            };
+
+            spriteBatch.Draw(TextureAssets.Npc[Type].Value, NPC.Center - screenPos, null, NPC.GetAlpha(drawColor), NPC.rotation, tex.Size() / 2, NPC.scale, SpriteEffects.None, 0);
+
+            // Desaturate in color when losing health, based on real anemones
+            float vibrance = MathHelper.Lerp(1, 0, Utils.GetLerpValue(1, 0.33f, NPC.life / (float)NPC.lifeMax, true));
+
+            spriteBatch.Draw(tex, NPC.Center - screenPos, null, NPC.GetAlpha(drawColor) * vibrance, NPC.rotation, tex.Size() / 2, NPC.scale, SpriteEffects.None, 0);
+
+            return false;
+        }
+    }
+}
---
+++ NPCs/SunkenSea/PolyperilTentacle.cs
@@ -0,0 +1,282 @@
+﻿using CalamityMod.DataStructures;
+using CalamityMod.Enums;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityMod.NPCs.SunkenSea
+{
+    public class PolyperilTentacle : SunkenSeaNPC
+    {
+        public ref float ParentIndex => ref NPC.ai[0];
+
+        public ref float Timer => ref NPC.ai[1];
+
+        // Tentacles are not a natural enemy, they are manually spawned by Polyperils
+        protected override SunkenSeaBiomeFlags BiomeDesignation => SunkenSeaBiomeFlags.None;
+
+        protected override float SpawningChance => 0;
+
+        public List<VerletSimulatedSegment> Segments;
+
+        // Where the tentacle will hover when not attacking
+        public Vector2 anchor;
+
+        protected override List<int> HuntNPCs => new List<int>()
+        {
+            ModContent.NPCType<SeaMinnow>(),
+            ModContent.NPCType<AlphaSeaMinnow>(),
+            ModContent.NPCType<SandProwler>(),
+            ModContent.NPCType<SandProwlerNested>(),
+            ModContent.NPCType<KelpieSeadragon>()
+        };
+        
+        protected override List<int> AvoidNPCs => new List<int>();
+        public override LocalizedText DisplayName => CalamityUtils.GetText("NPCs.Polyperil.DisplayName");
+
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+        }
+
+        public override void SetDefaults()
+        {
+            base.SetDefaults();
+
+            NPC.damage = 20;
+            NPC.lifeMax = 200;
+            NPC.defense = 0;
+            NPC.knockBackResist = 0f;
+            NPC.chaseable = false;
+            NPC.dontTakeDamage = true;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+
+            NPC.aiStyle = -1;
+            AIType = -1;
+            NPC.width = 20;
+            NPC.height = 20;
+
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.value = 0;
+            // Banner = NPC.type;
+            // BannerItem = ModContent.ItemType<PolyperilBanner>();
+        }
+
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(anchor.X);
+            writer.Write(anchor.Y);
+        }
+
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            anchor = new Vector2(reader.ReadSingle(), reader.ReadSingle());
+        }
+
+        protected override void BehaviorOnSpawn()
+        {
+            DetectionDistance = 620f;
+            ConeDetectionAngle = MathHelper.ToRadians(60f);
+        }
+
+        protected override void CreatureAI()
+        {
+            ConeDetectionDirection = NPC.rotation.ToRotationVector2();
+
+            NPC parent = Main.npc[(int)ParentIndex];
+            if (parent == null || !parent.active || parent.life < 0 || parent.type != ModContent.NPCType<Polyperil>())
+            {
+                NPC.active = false;
+                return;
+            }
+
+            // Create a chain
+            if (Segments == null || Segments.Count < 10)
+            {
+                Segments = new List<VerletSimulatedSegment>(10);
+                for (int i = 0; i < 10; i++)
+                {
+                    VerletSimulatedSegment segment = new VerletSimulatedSegment(parent.Center + Vector2.UnitY * i * 10);
+                    Segments.Add(segment);
+                }
+
+                Segments[0].locked = true;
+                Segments[^1].locked = true;
+            }
+
+            // While aggro'd, launch at the target then retreat
+            if (HasAnyTargets)
+            {
+                // Remain stationary while at rest
+                if (Timer < 0)
+                {
+                    NPC.velocity = Vector2.Zero;
+                }
+                // Launch
+                if (Timer == 0)
+                {
+                    NPC.velocity = NPC.SafeDirectionTo(NearestEntity.Center) * 8;
+                }
+                // Retreat
+                if (Timer > 20)
+                {
+                    NPC.velocity = NPC.SafeDirectionTo(anchor) * 5;
+                    if (Timer == 21)
+                    {
+                        RecalculatePosition(parent.Center);
+                    }
+                }
+                // Reset timer
+                if (Timer > 30 && NPC.Distance(anchor) < 20)
+                {
+                    NPC.velocity = Vector2.Zero;
+                    Timer = -120;
+                }
+            }
+            // Otherwise hover around the anchor position
+            else
+            {
+                NPC.velocity = NPC.SafeDirectionTo(anchor) * 3;
+                // Randomly relocate position
+                if (Timer > 300 && Main.rand.NextBool(180))
+                {
+                    RecalculatePosition(parent.Center);
+                    Timer = 0;
+                }
+            }
+            // If the anchor is too far from the parent (usually happens if the parent is moved) recalculate the anchor position
+            if (anchor.Distance(parent.Center) > 200)
+            {
+                RecalculatePosition(parent.Center);
+            }
+
+            // Repel other tentacles
+            float SAImovement = 0.05f;
+            for (int k = 0; k < Main.maxNPCs; k++)
+            {
+                NPC otherTentacle = Main.npc[k];
+                // Short circuits to make the loop as fast as possible
+                if (!otherTentacle.active || k == NPC.whoAmI || (otherTentacle.type != ModContent.NPCType<PolyperilTentacle>()))
+                    continue;
+
+                float taxicabDist = Math.Abs(NPC.position.X - otherTentacle.position.X) + Math.Abs(NPC.position.Y - otherTentacle.position.Y);
+                if (taxicabDist < NPC.width * 1.5f)
+                {
+                    if (NPC.position.X < otherTentacle.position.X)
+                        NPC.velocity.X -= SAImovement;
+                    else
+                        NPC.velocity.X += SAImovement;
+
+                    if (NPC.position.Y < otherTentacle.position.Y)
+                        NPC.velocity.Y -= SAImovement;
+                    else
+                        NPC.velocity.Y += SAImovement;
+                }
+            }
+            NPC.rotation = NPC.DirectionTo(parent.Center).ToRotation();
+
+            // Update the chain
+            Segments[0].oldPosition = Segments[0].position;
+            Segments[0].position = parent.Center;
+
+            Segments[Segments.Count - 1].oldPosition = Segments[Segments.Count - 1].position;
+            Segments[Segments.Count - 1].position = parent.active ? NPC.Center : parent.Center;
+
+            Segments = VerletSimulatedSegment.SimpleSimulation(Segments, 5, loops: 1, gravity: 0.3f);
+
+            NPC.netUpdate = true;
+            NPC.netSpam = 0;
+
+            Timer++;
+        }
+
+        public void RecalculatePosition(Vector2 basePos, int dist = 50)
+        {
+            anchor = basePos + new Vector2(Main.rand.Next(-dist, dist), Main.rand.Next(-dist, dist));
+        }
+
+        protected override void OnPlayerDetection(Player player)
+        {
+            Timer = 0;
+        }
+
+        protected override void OnPreyDetection(NPC prey)
+        {
+            Timer = 0;
+        }
+
+        protected override bool NPCSearchFilter(NPC n)
+        {
+            return base.NPCSearchFilter(n);
+        }
+
+        protected override bool PlayerSearchFilter(Player p)
+        {
+            return base.PlayerSearchFilter(p);
+        }
+
+        public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            NPC parent = Main.npc[(int)ParentIndex];
+            if (parent == null || !parent.active || parent.life < 0 || parent.type != ModContent.NPCType<Polyperil>())
+            {
+                return false;
+            }
+
+            // Color is based on parent color
+            Color col = parent.ai[1] switch
+            {
+                1 => new Color(27, 233, 255),
+                2 => new Color(7, 255, 204),
+                3 => Color.Cyan,
+                _ => new Color(255, 220, 248)
+            };
+
+            // Desaturate as parent's health lowers
+            float vibrance = MathHelper.Lerp(1, 0, Utils.GetLerpValue(1, 0.33f, parent.life / (float)parent.lifeMax, true));
+            Color final = Color.Lerp(Color.White, col, vibrance);
+
+            Texture2D tex = TextureAssets.Npc[Type].Value;
+            Rectangle tip = new Rectangle(0, 0, 8, 10);
+            Rectangle chain = new Rectangle(0, 12, 8, 8);
+            
+            if (Segments == null || Segments.Count <= 0)
+            {
+                spriteBatch.Draw(tex, NPC.Center - screenPos, tip, drawColor.MultiplyRGB(final), NPC.rotation, new Vector2(4, 10), NPC.scale, SpriteEffects.None, 0);
+                return false;
+            }
+
+            // Draw tentacle chains
+            for (int i = 0; i < Segments.Count - 1; i++)
+            {
+                VerletSimulatedSegment seg = Segments[i];
+                float dist = i > 0 ? Vector2.Distance(seg.position, Segments[i - 1].position) : 0;
+                if (dist <= 2)
+                    dist = 2;
+                dist += 4;
+                if (i == Segments.Count - 1)
+                {
+                    dist = Vector2.Distance(seg.position, parent.Center) + 2;
+                }
+                float rot = 0f;
+                if (i > 0)
+                    rot = seg.position.DirectionTo(Segments[i - 1].position).ToRotation();
+                SpriteEffects dir = NPC.Center.X > parent.Center.X ? SpriteEffects.FlipVertically : SpriteEffects.None;
+                spriteBatch.Draw(tex, seg.position - screenPos, chain, drawColor.MultiplyRGB(final), rot + MathHelper.PiOver2, new Vector2(4, 4), new Vector2(1, dist / 8), dir, 0);
+            }            
+
+            // Draw the tip
+            spriteBatch.Draw(tex, NPC.Center - screenPos, tip, drawColor.MultiplyRGB(final), NPC.rotation - MathHelper.PiOver2, new Vector2(4, 5), NPC.scale, SpriteEffects.None, 0);
+            return false;
+        }
+    }
+}
