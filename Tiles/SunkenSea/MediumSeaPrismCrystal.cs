54ecf961065955c02e3229ebfc6ae6d503ec15f9
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2025-02-10 19:28:15 -0700

    oops

--- Tiles/SunkenSea/MediumSeaPrismCrystal.cs
+++ Tiles/SunkenSea/MediumSeaPrismCrystal.cs
@@ -82,5 +82,9 @@ namespace CalamityMod.Tiles.SunkenSea
             g *= (value.G + value1.G) / 300f;
             b *= (value.B + value1.B) / 300f;
         }
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<PrismShard>(), 4);
+        }
     }
 }
