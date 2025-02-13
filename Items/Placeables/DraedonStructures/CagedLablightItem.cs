cdd8e975098559723f8e307a7be6d49079dfadba
Heart-Plus-Up <85328817+Heart-Plus-Up@users.noreply.github.com>
2025-02-12 18:52:52 -0700

    CagedLablights

---
+++ Items/Placeables/DraedonStructures/CagedLablightItem.cs
@@ -0,0 +1,28 @@
+﻿using CalamityMod.Items.DraedonMisc;
+using CalamityMod.Items.Materials;
+using CalamityMod.Tiles.DraedonStructures;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+namespace CalamityMod.Items.Placeables.DraedonStructures
+{
+    public class CagedLablightItem : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables.DraedonStructures";
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<CagedLablight>());
+            Item.value = Item.sellPrice(silver: 1);
+        }
+
+        //public override void AddRecipes()
+        //{
+        //    CreateRecipe().
+        //        AddIngredient<RustedPlating>(10).
+        //        AddIngredient<MysteriousCircuitry>().
+        //        AddIngredient<DraedonPowerCell>(8).
+        //        AddTile(TileID.Anvils).
+        //        Register();
+        //}
+    }
+}
