37d71b79e59dc74de66135ef84b6b46591a26d15
enamoured_ <xmmekxmwmxl@gmail.com>
2025-02-13 14:44:18 -0600

    multitile breaking correct item...

--- Tiles/DraedonStructures/CagedLights/AgedFloodlight.cs
+++ Tiles/DraedonStructures/CagedLights/AgedFloodlight.cs
@@ -87,7 +87,7 @@ namespace CalamityMod.Tiles.DraedonStructures.CagedLights
 
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
         {
-            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<CagedLablightItem>());
+            Item.NewItem(new EntitySource_TileBreak(i, j), i * 16, j * 16, 32, 48, ModContent.ItemType<AgedFloodlightItem>());
         }
     }
 }
