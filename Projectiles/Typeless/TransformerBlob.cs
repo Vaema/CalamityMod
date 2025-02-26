94e2b8aa2fabec9b9854b076a845a1e9f206ae65
Xyk <MetroidWhisperer@gmail.com>
2025-02-25 17:33:33 -0800

    Transformer MP fixes

--- Projectiles/Typeless/TransformerBlob.cs
+++ Projectiles/Typeless/TransformerBlob.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using CalamityMod.CalPlayer;
 using CalamityMod.Dusts;
 using CalamityMod.Particles;
@@ -6,7 +7,9 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.Audio;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
 
 namespace CalamityMod.Projectiles.Typeless
 {
@@ -34,9 +37,32 @@ namespace CalamityMod.Projectiles.Typeless
         public Color cl1 = Color.LightSkyBlue;
         public Color cl2 = Color.DodgerBlue;
         public float poweredLerp => (float)Math.Pow(Utils.GetLerpValue(poweredTimerMax, 90, poweredTimer, true), 4);
+        public override void SendExtraAI(BinaryWriter writer)
+        {
+            writer.Write(time);
+            writer.Write(currentLayer);
+            writer.Write(speed);
+            writer.Write(rotSpeed);
+            writer.Write(rotationAngle);
+            writer.Write(Projectile.localAI[0]);
+
+            writer.WriteFlags(canDamage);
+        }
+        public override void ReceiveExtraAI(BinaryReader reader)
+        {
+            time = reader.Read();
+            currentLayer = reader.Read();
+            speed = reader.ReadSingle();
+            rotSpeed = reader.ReadSingle();
+            rotationAngle = reader.ReadSingle();
+            Projectile.localAI[0] = reader.ReadSingle();
+
+            reader.ReadFlags(out canDamage);
+        }
         public override void SetStaticDefaults()
         {
             Main.projFrames[Type] = 16;
+            ProjectileID.Sets.NoLiquidDistortion[Type] = true;
         }
         public override void SetDefaults()
         {
@@ -56,6 +82,9 @@ namespace CalamityMod.Projectiles.Typeless
         public override void AI()
         {
             Player Owner = Main.player[Projectile.owner];
+
+            if (time % (Owner.ownedProjectileCounts[ModContent.ProjectileType<TransformerBlob>()] + 1) == 0)
+                Projectile.netUpdate = true;
 
             Lighting.AddLight(Projectile.Center, cl1.ToVector3() * 0.5f);
 
