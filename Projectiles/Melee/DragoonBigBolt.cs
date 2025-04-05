1cb1485db65fe5078de3f42d32f467fb13f41e5b
Xyk <MetroidWhisperer@gmail.com>
2025-04-05 01:12:17 -0700

    Adjusted Phantasmal Fury and Skytide Dragoon
    Phantasmal Fury projectiles should now ignore untargetable enemies if they are close to a targetable one
    Skytide dragoon bolts now always hit immediately when spawned if the enemy is between the player and the tip of the spear
    Buffed Skytide Dragoon a bit

--- Projectiles/Melee/DragoonBigBolt.cs
+++ Projectiles/Melee/DragoonBigBolt.cs
@@ -131,7 +131,18 @@ namespace CalamityMod.Projectiles.Melee
                 target.MoveNPC(launchVel, 20, true);
             }
         }
-        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) => CalamityUtils.CircularHitboxCollision(Projectile.Center, 45 * sizeMult * (Projectile.numHits > 0 ? 6 : 1), targetHitbox);
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            float size = 45 * sizeMult * (Projectile.numHits > 0 ? 6 : 1);
+            Player Owner = Main.player[Projectile.owner];
+            if (time <= 1 && Projectile.ai[1] != 0.5f)
+            {
+                float _ = float.NaN;
+                return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center, Owner.Center, size, ref _);
+            }
+            else
+                return CalamityUtils.CircularHitboxCollision(Projectile.Center, size, targetHitbox);
+        }
         public override bool? CanCutTiles() => false;
     }
 }
