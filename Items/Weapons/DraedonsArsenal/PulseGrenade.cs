53f5365709de866447860399cf15356992fda739
Xyk <MetroidWhisperer@gmail.com>
2025-04-24 00:20:38 -0700

    Frequency Manipulator -> Pulse Grenade
    Reworks Frequency Manipulator into the R-PLS: Pulse Grenade

--- Items/Weapons/DraedonsArsenal/PulseGrenade.cs
+++ Items/Weapons/DraedonsArsenal/PulseGrenade.cs
@@ -5,6 +5,7 @@ using CalamityMod.Items.Materials;
 using CalamityMod.Items.Weapons.Rogue;
 using CalamityMod.Projectiles.DraedonsArsenal;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -12,50 +13,56 @@ using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.DraedonsArsenal
 {
-    public class FrequencyManipulator : RogueWeapon, ILocalizedModType
+    [LegacyName("FrequencyManipulator")]
+    public class PulseGrenade : RogueWeapon, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.DraedonsArsenal";
         public override void SetDefaults()
         {
             CalamityGlobalItem modItem = Item.Calamity();
 
-            Item.width = 26;
-            Item.height = 44;
-            Item.damage = 80;
+            Item.width = 30;
+            Item.height = 40;
+            Item.damage = 59;
             Item.DamageType = RogueDamageClass.Instance;
             Item.noMelee = true;
             Item.noUseGraphic = true;
-            Item.useTime = 56;
-            Item.useAnimation = 56;
+            Item.useTime = 75;
+            Item.useAnimation = 75;
             Item.autoReuse = true;
             Item.useStyle = ItemUseStyleID.Swing;
-            Item.knockBack = 5f;
+            Item.knockBack = 8f;
+            Item.channel = true;
 
             Item.value = CalamityGlobalItem.RarityPinkBuyPrice;
             Item.rare = ItemRarityID.Pink;
-            Item.UseSound = SoundID.Item1;
 
             Item.shootSpeed = 16f;
-            Item.shoot = ModContent.ProjectileType<FrequencyManipulatorProjectile>();
-
-            modItem.UsesCharge = true;
-            modItem.MaxCharge = 85f;
-            modItem.ChargePerUse = 0.04f;
+            Item.shoot = ModContent.ProjectileType<PulseGrenadeProjectile>();
         }
+        public override bool CanUseItem(Player player)
+        {
+            return (player.ownedProjectileCounts[Item.shoot] <= 0 && player.ownedProjectileCounts[ModContent.ProjectileType<PulseGrenadeOrb>()] <= 0);
+        }
+        public override void HoldItem(Player player)
+        {
+            bool notUsingItem = (player.ownedProjectileCounts[Item.shoot] <= 0 && player.ownedProjectileCounts[ModContent.ProjectileType<PulseGrenadeOrb>()] <= 0);
+            if (!notUsingItem)
+            {
+                player.Calamity().rogueStealth = 0;
+            }
 
-        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
-
+            player.Calamity().mouseWorldListener = true;
+        }
         public override void ModifyTooltips(List<TooltipLine> tooltips) => CalamityGlobalItem.InsertKnowledgeTooltip(tooltips, 2);
-
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            if (player.Calamity().StealthStrikeAvailable())
-                damage = (int)(damage * 0.9f);
-
-            int proj = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
-            if (proj.WithinBounds(Main.maxProjectiles))
-                Main.projectile[proj].Calamity().stealthStrike = player.Calamity().StealthStrikeAvailable();
+            Projectile grenade = Projectile.NewProjectileDirect(source, position, velocity, type, damage, 0, player.whoAmI, 0, 0);
             return false;
+        }
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+        {
+            Item.DrawItemGlowmaskSingleFrame(spriteBatch, rotation, ModContent.Request<Texture2D>("CalamityMod/Items/Weapons/DraedonsArsenal/PulseGrenadeGlow").Value);
         }
 
         public override void AddRecipes()
