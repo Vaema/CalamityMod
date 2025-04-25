fbbd319f4e2e2a5601ddb675fa81802ae07dbdea
Doze-Zoze <dzc.1.dz@gmail.com>
2025-04-25 16:21:18 -0500

    gave sang. flare laceration and prelim dmg changes

--- Items/Weapons/Melee/TheMutilator.cs
+++ Items/Weapons/Melee/TheMutilator.cs
@@ -25,7 +25,7 @@ namespace CalamityMod.Items.Weapons.Melee
         {
             Item.width = 90;
             Item.height = 90;
-            Item.damage = 483;
+            Item.damage = 1111;
             Item.DamageType = DamageClass.Melee;
             Item.useAnimation = 30;
             Item.useStyle = ItemUseStyleID.Swing;
