51dc1caeef861b80b89f7840c78b2c0e2a3405b2
Xyk <MetroidWhisperer@gmail.com>
2025-04-25 23:09:20 -0700

    Good Visuals

--- Projectiles/Typeless/DiamondOfTheDeepProjectile.cs
+++ Projectiles/Typeless/DiamondOfTheDeepProjectile.cs
@@ -6,6 +6,7 @@ using CalamityMod.Buffs.Summon;
 using CalamityMod.CalPlayer;
 using CalamityMod.Dusts;
 using CalamityMod.Particles;
+using Microsoft.Build.Construction;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using ReLogic.Content;
@@ -57,7 +58,7 @@ namespace CalamityMod.Projectiles.Typeless
             if (time == 0 && idle)
             {
                 Main.NewText(projType);
-                switch (projType) // 1 = gravel/vines, 2 = hadal/hydrothermic, 3 = void/lumenyl
+                switch (projType) // 0 = gravel/vines, 1 = hadal/hydrothermic, 2 = void/lumenyl
                 {
                     case 0:
                         {
@@ -115,6 +116,48 @@ namespace CalamityMod.Projectiles.Typeless
             if (SoundEngine.TryGetActiveSound(soundSlot, out var sound) && sound.IsPlaying)
                 sound.Position = Projectile.Center;
 
+            float velLerp = Utils.GetLerpValue(-1.5f, 3f, Projectile.velocity.Length(), true);
+            if (projType == 0)
+            {
+                if (Main.rand.NextBool((int)(14 - 5 * velLerp)))
+                {
+                    Dust c = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(6, 6) * Projectile.scale + Projectile.velocity.SafeNormalize(Vector2.UnitX) * 15, ModContent.DustType<SquashDustHollow>());
+                    c.velocity = -Projectile.velocity * Main.rand.NextFloat(0.3f, 1f) * velLerp;
+                    c.scale = Main.rand.NextFloat(1f, 1.3f) * velLerp;
+                    c.noGravity = true;
+                    c.color = Main.rand.NextBool() ? Color.Aquamarine : bColor;
+                    c.noLightEmittence = true;
+                    c.fadeIn = 1;
+                    c.alpha = 100;
+                }
+            }
+            if (projType == 1)
+            {
+                if (Main.rand.NextBool((int)(10 - 5 * velLerp)))
+                {
+                    Dust c = Dust.NewDustPerfect(Projectile.Center + Projectile.velocity.SafeNormalize(Vector2.UnitX) * 15, ModContent.DustType<SquashDust>());
+                    c.velocity = -Projectile.velocity.RotatedByRandom(0.3f) * Main.rand.NextFloat(0.6f, 1.3f) * velLerp;
+                    c.scale = Main.rand.NextFloat(1.2f, 1.5f) * velLerp;
+                    c.noGravity = true;
+                    c.color = bColor;
+                    c.noLightEmittence = true;
+                    c.noGravity = !Main.rand.NextBool(3);
+                }
+            }
+            if (projType == 2)
+            {
+                if (Main.rand.NextBool((int)(3 - velLerp)))
+                {
+                    Dust c = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(6, 6) * Projectile.scale + Projectile.velocity.SafeNormalize(Vector2.UnitX) * 15, ModContent.DustType<SquashDust>());
+                    c.velocity = -Projectile.velocity.RotatedByRandom(0.1f) * Main.rand.NextFloat(1.4f, 3.5f) * velLerp;
+                    c.scale = Main.rand.NextFloat(1.8f, 2.3f) * velLerp;
+                    c.noGravity = true;
+                    c.color = bColor;
+                    c.noLightEmittence = true;
+                    c.fadeIn = 2.3f;
+                }
+            }
+
             if (idle) // Following Ai
             {
                 if (time == 0 && visuals) // On spawn fx
@@ -129,21 +172,27 @@ namespace CalamityMod.Projectiles.Typeless
                         dust2.scale = scale * 3;
                         dust2.noGravity = false;
                         dust2.alpha = 180;
-                        dust2.color = Main.rand.NextBool(4) ? Color.Lerp(Color.Yellow, bColor, 0.5f) : bColor;
+                        dust2.color = bColor;
                         dust2.noLight = true;
                         dust2.noLightEmittence = true;
                     }
                 }
                 if (time > 80) // Track the player in a natural motion simular to swimming
                 {
-                    float homingSpeed = Utils.Remap(Utils.Distance(Projectile.Center, Owner.Center), 200, 600, 0.07f, 0.16f);
+                    float homingSpeed = Utils.Remap(Utils.Distance(Projectile.Center, Owner.Center), 200, 600, 0.07f, 0.16f) + 0.009f * energyNumber;
                     float offsetPower = Utils.GetLerpValue(1, 5, Owner.velocity.Length(), true);
                     float sine = (float)Math.Sin((time * 0.1f) / MathHelper.Pi);
                     float sine2 = (float)Math.Sin((time * 0.04f) / MathHelper.Pi);
-                    Vector2 bonusMobility = (offsetPower > 0 ? ((Utils.DirectionTo(Projectile.Center, Owner.Center) * 50) * sine2).RotatedBy(0.8f * sine) * offsetPower : Vector2.Zero);
-                    Vector2 goalPosition = Owner.MountedCenter + ((MathHelper.TwoPi * energyNumber) / Math.Max(Owner.ownedProjectileCounts[ModContent.ProjectileType<DiamondOfTheDeepProjectile>()], 1)).ToRotationVector2().RotatedBy(Main.GlobalTimeWrappedHourly * 0.4f) * 180;
+                    Vector2 bonusMobility = (offsetPower > 0 ? ((Utils.DirectionTo(Projectile.Center, Owner.Center) * 90) * sine2).RotatedBy(0.8f * sine) * offsetPower : Vector2.Zero);
+                    Vector2 goalPosition = Owner.MountedCenter + bonusMobility + ((MathHelper.TwoPi * energyNumber) / Math.Max(Owner.ownedProjectileCounts[ModContent.ProjectileType<AmuletEnergy>()], 1)).ToRotationVector2().RotatedBy(Main.GlobalTimeWrappedHourly * 0.4f) * 20;
 
-                    Projectile.velocity = Projectile.velocity * 0.995f + Utils.DirectionTo(Projectile.Center, goalPosition) * homingSpeed;
+                    bool outOfRange = Utils.Distance(Projectile.Center, goalPosition) > 120;
+                    if (Projectile.velocity.Length() < 6 && outOfRange)
+                        Projectile.velocity = Projectile.velocity * 0.995f + Utils.DirectionTo(Projectile.Center, goalPosition) * homingSpeed;
+                    else if (outOfRange)
+                        Projectile.velocity *= 0.985f;
+                    if (!outOfRange)
+                        Projectile.velocity = Projectile.velocity.RotatedBy(0.0065f * (energyNumber % 2 == 0 ? -1 : 1)) * 1.004f;
                 }
                 else
                     Projectile.velocity *= 0.99f;
@@ -213,11 +262,11 @@ namespace CalamityMod.Projectiles.Typeless
             float squash = Utils.GetLerpValue(1, 3, Projectile.velocity.Length(), true);
             if (squash > 0.15f && visuals)
             {
-                Particle fadeInfx = new CustomSpark(Projectile.Center, Projectile.velocity * 0.01f, "CalamityMod/Particles/BloomCircle", false, 18, 0.55f * Projectile.scale, bColor * 0.3f * squash, new Vector2(1 - 0.15f * squash, 1f), true, false, shrinkSpeed: 0.3f * squash);
+                Particle fadeInfx = new CustomSpark(Projectile.Center, Projectile.velocity * 0.01f, "CalamityMod/Particles/BloomCircle", false, 18, 0.55f * Projectile.scale, bColor * 0.4f * squash, new Vector2(1 - 0.15f * squash, 1f), true, true, shrinkSpeed: 0.3f * squash, glowOpacity: 0.5f, glowCenterScale: 0.45f);
                 GeneralParticleHandler.SpawnParticle(fadeInfx);
             }
 
-            Projectile.scale = MathHelper.Lerp(Projectile.scale, 1, 0.1f);
+            Projectile.scale = MathHelper.Lerp(Projectile.scale, 0.7f, 0.1f);
             Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver2;
             time++;
         }
@@ -235,7 +284,7 @@ namespace CalamityMod.Projectiles.Typeless
                     Vector2 vel = (Projectile.velocity.SafeNormalize(Vector2.UnitX) * 4).RotatedBy(variance) * Main.rand.NextFloat(0.3f, 1f) * (1 - Math.Abs(variance)) * 4;
                     float scale = (Main.rand.NextFloat(1.5f, 1.7f) - Math.Abs(variance)) * 0.35f;
 
-                    Particle sparks = new CustomSpark(Projectile.Center + vel, vel, "CalamityMod/Particles/BloomCircle", false, Main.rand.Next(13, 16 + 1), scale, bColor * 0.7f, new Vector2(1f, 1), true, false, 0, false, shrinkSpeed: 0.25f);
+                    Particle sparks = new CustomSpark(Projectile.Center + vel, vel, "CalamityMod/Particles/BloomCircle", false, Main.rand.Next(13, 16 + 1), scale, bColor * 0.7f, new Vector2(1f, 1), true, true, 0, false, shrinkSpeed: 0.25f);
                     GeneralParticleHandler.SpawnParticle(sparks);
 
                     Dust dust2 = Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<LightDust>(), vel);
@@ -255,7 +304,7 @@ namespace CalamityMod.Projectiles.Typeless
             Vector2 squash = new Vector2(Utils.Remap(Projectile.velocity.Length(), 1, 5, 1, 0.6f), Utils.Remap(Projectile.velocity.Length(), 1, 5, 1, 2f));
 
             Asset<Texture2D> block = ModContent.Request<Texture2D>("CalamityMod/Items/Placeables/Abyss/AbyssGravel");
-            switch (projType) // 1 = gravel/vines, 2 = hadal/hydrothermic, 3 = void/lumenyl
+            switch (projType) // 0 = gravel/vines, 1 = hadal/hydrothermic, 2 = void/lumenyl
             {
                 case 0:
                     {
@@ -278,26 +327,72 @@ namespace CalamityMod.Projectiles.Typeless
             {
                 Color orbColor = Color.Lerp(bColor, color2, (i + 1) / 6) with { A = 0 } * 0.4f * (visuals ? 1 : 0.1f);
                 Vector2 scale = Projectile.scale * squash * (0.05f + i * 0.01f) * 4.3f;
-                Main.EntitySpriteDraw(orb.Value, Projectile.Center - Main.screenPosition, null, orbColor, Projectile.rotation, orb.Size() * 0.5f, scale, SpriteEffects.None);
+                Main.EntitySpriteDraw(orb.Value, Projectile.Center - Main.screenPosition, null, Color.Lerp(orbColor, Color.White with { A = 0 }, 1 - i * 0.5f), Projectile.rotation, orb.Size() * 0.5f, scale, SpriteEffects.None);
             }
 
-            int orbs = 8;
-            for (int i = 1; i < orbs + 1; i++)
+            float velLerp = Utils.GetLerpValue(0.5f, 3f, Projectile.velocity.Length(), true);
+            if (projType == 0)
             {
-                float sine3 = (float)Math.Sin((time * (i % 3 == 0 ? 0.6f : i % 2 == 0 ? 0.8f : 1f) * 0.07f) / MathHelper.Pi);
-                float sine2 = (float)Math.Sin((time * (i % 3 == 0 ? 0.6f : i % 2 == 0 ? 0.8f : 1f) * 0.2f) / MathHelper.Pi);
-
-                Vector2 velocity = ((MathHelper.TwoPi * i / orbs) + time * 0.04f).ToRotationVector2() * (43f + 8 * sine3) * Math.Abs(sine2);
-
-                Color orbColor = Color.White * (visuals ? 1 : 0.1f);
-                Vector2 scale = Vector2.One * Projectile.scale;
-
-                for (int y = 0; y < 7; y++)
+                Asset<Texture2D> ring = ModContent.Request<Texture2D>("CalamityMod/Particles/BloomRing");
+                int orbs = 9;
+                for (int i = 1; i < orbs + 1; i++)
                 {
-                    Vector2 drawOffset = (MathHelper.TwoPi * y / 7).ToRotationVector2() * 4.5f;
-                    Main.EntitySpriteDraw(block.Value, Projectile.Center - Main.screenPosition + velocity + drawOffset, null, bColor with { A = 0 } * 0.7f, Projectile.rotation + 0.02f * MathHelper.Lerp(i, 1, 0.75f) + 0.4f * sine3, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                    float sine2 = (float)Math.Sin((time * (i % 3 == 0 ? 0.2f : i % 2 == 0 ? 0.4f : 1f) * 0.2f) / MathHelper.Pi);
+
+                    Vector2 placement = Projectile.Center + ((MathHelper.TwoPi * i / orbs) + 7 * sine).ToRotationVector2() * (18 + Math.Abs(sine * (i % 3 == 0 ? 19 : 11))) * velLerp;
+                    Color orbColor = Color.White * (visuals ? 1 : 0.1f) * velLerp;
+                    Vector2 scale = Vector2.One * Projectile.scale;
+
+                    for (int y = 0; y < 7; y++)
+                    {
+                        Vector2 drawOffset = (MathHelper.TwoPi * y / 7).ToRotationVector2() * 4.5f;
+                        Main.EntitySpriteDraw(block.Value, placement - Main.screenPosition + drawOffset, null, bColor with { A = 0 } * 0.7f * velLerp, Projectile.rotation + 0.02f * MathHelper.Lerp(i, 1, 0.75f) + 0.4f, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                    }
+                    Main.EntitySpriteDraw(block.Value, placement - Main.screenPosition, null, orbColor, Projectile.rotation + 0.02f * MathHelper.Lerp(i, 1, 0.75f) + 0.4f, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
                 }
-                Main.EntitySpriteDraw(block.Value, Projectile.Center - Main.screenPosition + velocity, null, orbColor, Projectile.rotation + 0.02f * MathHelper.Lerp(i, 1, 0.75f) + 0.4f * sine3, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                for (int i = 0; i < 3; i++)
+                    Main.EntitySpriteDraw(ring.Value, Projectile.Center - Main.screenPosition, null, bColor with { A = 0 } * 0.9f * velLerp, Projectile.rotation, ring.Size() * 0.5f, 0.2f * velLerp * Projectile.scale + (0.15f * Math.Abs(sine)) + 0.02f * i, SpriteEffects.None);
+            }
+            if (projType == 1)
+            {
+                int orbs = 9;
+                for (int i = 0; i < orbs; i++)
+                {
+                    bool outer = i > (2);
+                    bool outest = i > (5);
+                    float rotation = MathHelper.TwoPi * i / (3) + Main.GlobalTimeWrappedHourly * 6;
+                    Vector2 placement = Projectile.Center + ((rotation).ToRotationVector2() * (outest ? 16 : outer ? 12 : 8) + (Projectile.velocity.SafeNormalize(Vector2.UnitX) * (outest ? -15f : outer ? 0f : 15))) * velLerp;
+                    Color orbColor = Color.White * (visuals ? 1 : 0.1f) * velLerp;
+                    Vector2 scale = new Vector2(0.35f + (outest ? 0.1f : outer ? 0.3f : 0.6f), 2.3f) * Projectile.scale * (outest ? 0.8f : outer ? 0.9f : 1) * 1.5f;
+
+                    for (int y = 0; y < 7; y++)
+                    {
+                        Vector2 drawOffset = (MathHelper.TwoPi * y / 7).ToRotationVector2() * 2.5f;
+                        Main.EntitySpriteDraw(block.Value, placement - Main.screenPosition + drawOffset, null, bColor with { A = 0 } * velLerp, Utils.DirectionTo(placement, Projectile.Center + Projectile.velocity.SafeNormalize(Vector2.UnitX) * 65f).ToRotation() + MathHelper.PiOver2, block.Size() * 0.5f, scale, outer ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                    }
+                    Main.EntitySpriteDraw(block.Value, placement - Main.screenPosition, null, orbColor, Utils.DirectionTo(placement, Projectile.Center + Projectile.velocity.SafeNormalize(Vector2.UnitX) * 65f).ToRotation() + MathHelper.PiOver2, block.Size() * 0.5f, scale, outer ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                }
+            }
+            if (projType == 2)
+            {
+                int orbs = 8;
+                for (int i = 1; i < orbs + 1; i++)
+                {
+                    float sine3 = (float)Math.Sin((time * (i % 3 == 0 ? 0.2f : i % 2 == 0 ? 0.4f : 1f) * 0.07f) / MathHelper.Pi);
+                    float sine2 = (float)Math.Sin((time * (i % 3 == 0 ? 0.2f : i % 2 == 0 ? 0.4f : 1f) * 0.2f) / MathHelper.Pi);
+
+                    Vector2 velocity = Projectile.Center + ((MathHelper.TwoPi * i / orbs) + time * 0.04f).ToRotationVector2() * (23f + 8 * sine3) * Math.Abs(sine2) * velLerp;
+
+                    Color orbColor = Color.White * (visuals ? 1 : 0.1f) * velLerp;
+                    Vector2 scale = new Vector2(i % 2 == 0 ? 0.4f : 0.6f, i % 2 == 0 ? 1f : 1.3f) * Projectile.scale * 1.3f;
+
+                    for (int y = 0; y < 7; y++)
+                    {
+                        Vector2 drawOffset = (MathHelper.TwoPi * y / 7).ToRotationVector2() * 4.5f;
+                        Main.EntitySpriteDraw(block.Value, velocity - Main.screenPosition + drawOffset, null, bColor with { A = 0 } * 0.7f * velLerp, Utils.DirectionTo(velocity, Projectile.Center).ToRotation() + MathHelper.PiOver2 * sine3, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                    }
+                    Main.EntitySpriteDraw(block.Value, velocity - Main.screenPosition, null, orbColor, Utils.DirectionTo(velocity, Projectile.Center).ToRotation() + MathHelper.PiOver2 * sine3, block.Size() * 0.5f, scale, i % 2 == 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                }
             }
 
             return false;
