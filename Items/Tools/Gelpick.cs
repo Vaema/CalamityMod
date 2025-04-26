1e39aefad32e077c72805549833be3c9324f5400
Altixal <73398287+Altixal@users.noreply.github.com>
2025-04-26 09:34:28 +0700

    monolith goes too

--- Items/Tools/Gelpick.cs
+++ Items/Tools/Gelpick.cs
@@ -16,9 +16,9 @@ namespace CalamityMod.Items.Tools
             Item.height = 48;
             Item.damage = 19;
             Item.knockBack = 2.5f;
-            Item.useTime = 9;
+            Item.useTime = 6;
             Item.useAnimation = 20;
-            Item.pick = 105;
+            Item.pick = 90;
             Item.tileBoost += 1;
 
             Item.DamageType = DamageClass.Melee;
