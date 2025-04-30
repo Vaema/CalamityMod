15ce3e77c94cc58ec85fa0b67cb102b186bf6ecf
Xyk <MetroidWhisperer@gmail.com>
2025-04-30 02:28:45 -0700

    Stormlion Sentry
    Sniper like antlion, charges and fires a fast electric projectile, but can't update it's aim very fast.
    These guys love music and will dance if you play any for them.

---
+++ Effects/StormlionEffects.cs
@@ -0,0 +1,24 @@
+﻿using CalamityMod.Dusts;
+using Microsoft.Xna.Framework;
+using Terraria.Audio;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Effects
+{
+    public class StormlionEffects
+    {
+        // Energy
+        public static int EnergyDust = ModContent.DustType<LightDust>();
+        public static Color EnergyColor = new Color(5, 187, 177);
+        // Flesh
+        public static int FleshDust = 192;
+        public static Color FleshColor = new Color(171, 113, 91);
+
+        // Sounds
+        public static readonly SoundStyle Hit = new("CalamityMod/Sounds/NPCHit/StormlionAltHit");
+        public static readonly SoundStyle Killed = new("CalamityMod/Sounds/NPCKilled/StormlionAltDeath");
+        public static readonly SoundStyle Attack = new("CalamityMod/Sounds/Custom/StormlionAltShoot");
+        public static readonly SoundStyle Idle1 = new("CalamityMod/Sounds/Custom/StormlionAltIdle1");
+        public static readonly SoundStyle Idle2 = new("CalamityMod/Sounds/Custom/StormlionAltIdle2");
+    }
+}
