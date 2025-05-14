202dbc6edc13c140f359c83f3d31f4783a5d17b8
Nathanael Overton <nsoverton@gmail.com>
2025-05-14 14:26:58 -0400

    Storm Weaver Charge Telegraph

--- Sounds/CommonCalamitySounds.cs
+++ Sounds/CommonCalamitySounds.cs
@@ -19,7 +19,9 @@ namespace CalamityMod.Sounds
 
         public static readonly SoundStyle WyrmScreamSound = new("CalamityMod/Sounds/Custom/WyrmScream");
 
-        public static readonly SoundStyle LightningSound = new("CalamityMod/Sounds/Custom/LightningStrike") { Volume = 1.5f }; //This is just the regular SoundID.Thunder sound, except its only the first variant
+        public static readonly SoundStyle LightningSound = new("CalamityMod/Sounds/Custom/LightningStrike") { Volume = 1.6f }; //This is just the regular SoundID.Thunder sound, except its only the first variant
+       
+        public static readonly SoundStyle LightningTelegraph = new("CalamityMod/Sounds/Custom/LightningTelegraph") { Volume = 0.7f }; //This is just the regular SoundID.Thunder sound, except its only the fifth variant
 
         public static readonly SoundStyle LaserCannonSound = new("CalamityMod/Sounds/Item/LaserCannon") { Volume = 0.85f };
 
