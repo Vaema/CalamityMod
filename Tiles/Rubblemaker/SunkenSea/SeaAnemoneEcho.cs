50f9cf77064a81c60218d7f421e86a549b5cfcb2
enamoured_ <xmmekxmwmxl@gmail.com>
2025-05-24 04:50:07 -0400

    pulsing light death pre-optimisation (see desc)
    killed pulsing light on the following:
    -phoviamare halm (abyss layer 3 grass - drowned out by the other light-emitting tiles)
    -sulphur tentacle coral (sulphur depths ambient tile - obscenely subtle and slow, drowned out by other light-emitting tiles)
    -all coral tiles (pulse is obscenely unnoticeable combined with water effects and other light)
    -sea anemone + echo (it wasnt doing anything. sunken sea is Made Of Light)
    -scarlet seagrass tile (it contributes literally nothing because of the water)
    
    removed all light from:
    -long scarlet seagrass (polyp forest grass, drowned out by the water light)
    -wide scarlet seagrass (got drowned out by the water)
    
    ADDITIONAL NOTE: scarlet seagrass tile didnt even set tileLighted to true lmao

--- Tiles/Rubblemaker/SunkenSea/SeaAnemoneEcho.cs
+++ Tiles/Rubblemaker/SunkenSea/SeaAnemoneEcho.cs
@@ -31,18 +31,9 @@ namespace CalamityMod.Tiles.Rubblemaker.SunkenSea
 
         public override void ModifyLight(int i, int j, ref float r, ref float g, ref float b)
         {
-            float brightness = 0.7f;
-            float declareThisHereToPreventRunningTheSameCalculationMultipleTimes = Main.GameUpdateCount * 0.01f;
-            brightness *= (float)MathF.Sin(-j / 8f + declareThisHereToPreventRunningTheSameCalculationMultipleTimes + i);
-            brightness *= (float)MathF.Sin(-i / 8f + declareThisHereToPreventRunningTheSameCalculationMultipleTimes + j);
-            brightness += 0.4f;
-            brightness = MathHelper.Clamp(brightness, 0.1f, 0.5f);
-            r = 0.33f;
-            b = 0.59f;
-            g = 0.77f;
-            r *= brightness;
-            g *= brightness;
-            b *= brightness;
+            r = 0.11f;
+            g = 0.29f;
+            b = 0.57f;
         }
 
         public override void NumDust(int i, int j, bool fail, ref int num)
