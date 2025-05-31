265ced5b9411a44762a4afb3a434a6106d5c1929
enamoured_ <xmmekxmwmxl@gmail.com>
2025-05-31 05:27:30 -0400

    ☼︎☜︎💣︎☜︎💣︎👌︎☜︎☼︎ 💧︎⚐︎☠︎📪︎ 👎︎✡︎✋︎☠︎☝︎ ✋︎💧︎ ☝︎✌︎✡︎
    what a nice happy family :)

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -203,6 +203,7 @@ namespace CalamityMod.NPCs
         {
             "Sans Undertale", // <@!145379091648872450> (shayy)
             "Papyrus Undertale", // <@!262663471189983242> (nycro)
+            "Gaster Undertale", // <@!924706306093379614> (enamoured)
             "Mr. Bones", // <@!359215912856977408> (jaybones.)
             "Freakbob", // <@!377863128140087296> (jevilamv)
         };
