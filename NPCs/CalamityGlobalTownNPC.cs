e594a73a25e52cd2f9975850984f4797143e0dac
Ozzatron <spazeglitch@gmail.com>
2025-06-02 14:10:04 -0400

    Add Zoologist name Daxie for donor

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -275,6 +275,7 @@ namespace CalamityMod.NPCs
             "Mae Borowski", //<@!219158690433990656> (justakkolite)
             "Fera", // <@!195850711567826945> (juneark_)
             "Gwenhwyvar", // <@!291342874497515531> (diamondnife)
+            "Daxie", // <@!465438861103988737> (daxie626)
         };
 
         // The following sets are for the 1.4 Town Pets: Town Dogs, Cats and Bunnies.
