f5def149772200164b2d71250ffb91b6b29fc430
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-06-05 22:13:55 -0400

    Added Disgusting Meat
    Unobtainable debug item which can remove permanent boosters, dropping them as items

---
+++ Items/DisgustingMeat.cs
@@ -0,0 +1,60 @@
+﻿using CalamityMod.CalPlayer;
+using CalamityMod.Items.PermanentBoosters;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items
+{
+    public class DisgustingMeat : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Misc";
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToFood(32, 30, 0, 0);
+            Item.value = 0;
+            Item.UseSound = SoundID.NPCDeath13;
+            Item.maxStack = 1;
+            Item.consumable = false;
+        }
+
+        public override bool AltFunctionUse(Player player) => true;
+        public override bool? UseItem(Player player)
+        {
+            var calPlayer = player.Calamity();
+            if (player.altFunctionUse == 2)
+            {
+                TryDropBoosterItem(player, ref calPlayer.rageBoostOne, ModContent.ItemType<MushroomPlasmaRoot>());
+                TryDropBoosterItem(player, ref calPlayer.rageBoostTwo, ModContent.ItemType<InfernalBlood>());
+                TryDropBoosterItem(player, ref calPlayer.rageBoostThree, ModContent.ItemType<RedLightningContainer>());
+                TryDropBoosterItem(player, ref calPlayer.adrenalineBoostOne, ModContent.ItemType<ElectrolyteGelPack>());
+                TryDropBoosterItem(player, ref calPlayer.adrenalineBoostTwo, ModContent.ItemType<StarlightFuelCell>());
+                TryDropBoosterItem(player, ref calPlayer.adrenalineBoostThree, ModContent.ItemType<Ectoheart>());
+                TryDropBoosterItem(player, ref calPlayer.nimbleBounderBoost, ModContent.ItemType<NimbleBounder>());
+            }
+            else
+            {
+                TryDropBoosterItem(player, ref calPlayer.sTangerine, ModContent.ItemType<SanguineTangerine>());
+                TryDropBoosterItem(player, ref calPlayer.mFruit, ModContent.ItemType<MiracleFruit>());
+                TryDropBoosterItem(player, ref calPlayer.tCloudberry, ModContent.ItemType<TaintedCloudberry>());
+                TryDropBoosterItem(player, ref calPlayer.sStrawberry, ModContent.ItemType<SacredStrawberry>());
+                TryDropBoosterItem(player, ref calPlayer.cShard, ModContent.ItemType<CometShard>());
+                TryDropBoosterItem(player, ref calPlayer.eCore, ModContent.ItemType<EtherealCore>());
+                TryDropBoosterItem(player, ref calPlayer.pHeart, ModContent.ItemType<PhantomHeart>());
+            }
+
+            return null;
+        }
+
+        private static void TryDropBoosterItem(Player player, ref bool condition, int itemType)
+        {
+            if (condition)
+            {
+                condition = false;
+                int drop = Item.NewItem(player.GetSource_DropAsItem(), player.Hitbox, itemType);
+                Main.item[drop].noGrabDelay = 100;
+            }
+        }
+    }
+}
