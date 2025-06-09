f90b4f463a7a9fecfcbbde1ddf2994d8dfe0b266
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-06-08 21:24:50 -0400

    Some Yharon tweaks
    1 - Changed a few phase thresholds to be more even
    2 - Slightly increased bullet hell flare dust velocity (This may seem like a straight buff, but it also means that they clear out sooner, resulting in less attack overlap)
    3 - Slightly decreased the density of slow bullet hells in SP6 and SP7

--- NPCs/Yharon/Yharon.cs
+++ NPCs/Yharon/Yharon.cs
@@ -221,9 +221,9 @@ namespace CalamityMod.NPCs.Yharon
             }
 
             // Phase booleans
-            float phase2GateValue = revenge ? 0.9f : expertMode ? 0.85f : 0.75f;
+            float phase2GateValue = revenge ? 0.9f : expertMode ? 0.85f : 0.8f;
             bool phase2Check = death || lifeRatio <= phase2GateValue;
-            bool phase3Check = lifeRatio <= (death ? 0.8f : revenge ? 0.75f : expertMode ? 0.7f : 0.625f);
+            bool phase3Check = lifeRatio <= (death ? 0.8f : revenge ? 0.75f : expertMode ? 0.7f : 0.65f);
             bool phase4Check = lifeRatio <= ai2GateValue;
             bool phase1Change = NPC.ai[0] > -1f;
             bool phase2Change = NPC.ai[0] > 5f;
@@ -1436,7 +1436,7 @@ namespace CalamityMod.NPCs.Yharon
 
                     if (Main.netMode != NetmodeID.MultiplayerClient)
                     {
-                        DoFlareDustBulletHell(1, flareDustPhaseTimer, NPC.GetProjectileDamage(ModContent.ProjectileType<FlareDust>()), 8, 12f, 3.6f, false);
+                        DoFlareDustBulletHell(1, flareDustPhaseTimer, NPC.GetProjectileDamage(ModContent.ProjectileType<FlareDust>()), 8, 13f, 3.6f, false);
                     }
                 }
 
@@ -1623,12 +1623,10 @@ namespace CalamityMod.NPCs.Yharon
         {
             CalamityGlobalNPC.yharonP2 = NPC.whoAmI;
 
-            float phase2GateValue = revenge ? 0.44f : expertMode ? 0.385f : 0.275f;
+            float phase2GateValue = revenge ? 0.45f : expertMode ? 0.35f : 0.3f;
             bool phase2 = death || lifeRatio <= phase2GateValue;
-            float phase3GateValue = death ? 0.358f : revenge ? 0.275f : expertMode ? 0.22f : 0.138f;
-            bool phase3 = lifeRatio <= phase3GateValue;
-            float phase4GateValue = death ? 0.165f : 0.11f;
-            bool phase4 = lifeRatio <= phase4GateValue && revenge;
+            bool phase3 = lifeRatio <= (death ? 0.35f : revenge ? 0.3f : expertMode ? 0.2f : 0.15f);
+            bool phase4 = lifeRatio <= (death ? 0.2f : 0.15f) && revenge;
 
             if (NPC.ai[0] != 5f && NPC.ai[0] != 8f)
             {
@@ -1786,7 +1784,7 @@ namespace CalamityMod.NPCs.Yharon
 
             int spinPhaseTimer = secondPhasePhase == 4 ? (bossRush ? 80 : death ? 100 : 120) : (bossRush ? 120 : death ? 150 : 180);
             int flareDustSpawnDivisor = spinPhaseTimer / 10;
-            int flareDustSpawnDivisor2 = spinPhaseTimer / 20 + (secondPhasePhase == 4 ? spinPhaseTimer / 60 : 0);
+            int flareDustSpawnDivisor2 = spinPhaseTimer / 15 + (secondPhasePhase == 4 ? spinPhaseTimer / 60 : 0);
 
             float increasedIdleTimeAfterBulletHell = (death ? 120f : 180f) + phaseSwitchTimer;
             bool moveSlowerAfterBulletHell = NPC.ai[1] < 0f;
@@ -2358,7 +2356,7 @@ namespace CalamityMod.NPCs.Yharon
                         if (NPC.ai[1] % flareDustSpawnDivisor2 == 0f)
                         {
                             int totalProjectiles = secondPhasePhase == 4 ? 12 : 10;
-                            float projectileVelocity = secondPhasePhase == 4 ? 16f : 12f;
+                            float projectileVelocity = secondPhasePhase == 4 ? 17f : 13f;
                             float radialOffset = secondPhasePhase == 4 ? 2.8f : 3.2f;
                             if (NPC.localAI[3] == 0f)
                             {
