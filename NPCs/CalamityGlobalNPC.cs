1fd5f28094b7a659ee6d6d5cde0b3c2c4f25400f
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-06-13 23:45:15 -0400

    Removed increased Voodoo Demon spawn rates

--- NPCs/CalamityGlobalNPC.cs
+++ NPCs/CalamityGlobalNPC.cs
@@ -7361,11 +7361,6 @@ namespace CalamityMod.NPCs
             // If the doll is active, Voodoo Demons cannot spawn (via modded means).
             if (voodooDemonDollActive)
                 pool.Remove(NPCID.VoodooDemon);
-            // Otherwise, if it's pre-Hardmode, provide a modded spawn entry that makes them much more common.
-            else if (!Main.hardMode && spawnInfo.Player.ZoneUnderworldHeight && !calamityBiomeZone)
-            {
-                pool[NPCID.VoodooDemon] = SpawnCondition.Underworld.Chance * 0.15f;
-            }
         }
         #endregion
 
