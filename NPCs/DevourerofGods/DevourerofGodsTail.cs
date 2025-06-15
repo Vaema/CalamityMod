053e3823df61969d57073683b00fb291ed2f6c52
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-06-15 10:43:18 -0400

    Body and tail no longer inflict debuffs

--- NPCs/DevourerofGods/DevourerofGodsTail.cs
+++ NPCs/DevourerofGods/DevourerofGodsTail.cs
@@ -477,14 +477,5 @@ namespace CalamityMod.NPCs.DevourerofGods
             NPC.lifeMax = (int)(NPC.lifeMax * 0.8f * balance * bossAdjustment);
             NPC.damage = (int)(NPC.damage * NPC.GetExpertDamageMultiplier());
         }
-
-        public override void OnHitPlayer(Player target, Player.HurtInfo hurtInfo)
-        {
-            if (hurtInfo.Damage > 0)
-            {
-                target.AddBuff(ModContent.BuffType<GodSlayerInferno>(), 120, true);
-                target.AddBuff(ModContent.BuffType<WhisperingDeath>(), 360, true);
-            }
-        }
     }
 }
