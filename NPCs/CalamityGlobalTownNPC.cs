50dfb855823189bec9f472188ec5d216b08b345c
Ozzatron <spazeglitch@gmail.com>
2025-07-03 10:12:23 -0400

    Add Orange Tabby Cat name Kiba for donor

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -358,6 +358,7 @@ namespace CalamityMod.NPCs
             "Felix", // <@!183424826407518208> (lilac_vrt_olligoci)
             "Tardo", // <@!739343546867384391> (midnight295)
             "Dali", // <@!460238880436781061> (darthlego)
+            "Kiba", // <@!852348657072340992> (jollydragonslayer)
         };
         private static readonly string[] TownCatRussianBlueNames = null;
         private static readonly string[] TownCatSilverNames =
