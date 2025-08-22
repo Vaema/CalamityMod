c28c63de14c28990da62c17403531e598431b89c
Altixal <73398287+Altixal@users.noreply.github.com>
2025-08-22 09:43:57 +0700

    removed statigel iframes small statigel summoner buff??

--- Utilities/PlayerUtils.cs
+++ Utilities/PlayerUtils.cs
@@ -9,7 +9,6 @@ using CalamityMod.Enums;
 using CalamityMod.Events;
 using CalamityMod.Items.Accessories;
 using CalamityMod.Items.Armor;
-using CalamityMod.Items.Armor.Statigel;
 using CalamityMod.Items.Potions.Alcohol;
 using CalamityMod.Systems.Collections;
 using CalamityMod.World;
@@ -439,8 +438,6 @@ namespace CalamityMod
             int extraIFrames = 0;
             if (modPlayer.godSlayerThrowing && hurtInfo.Damage > 80)
                 extraIFrames += 30;
-            if (modPlayer.statigelSet && hurtInfo.Damage > StatigelArmor.SetBonusHurtDamageThreshold)
-                extraIFrames += StatigelArmor.SetBonusIFrameExtension;
 
             // Deific Amulet provides 10 to 40 bonus immunity frames when you get hit which scale with your missing health.
             // If you only take 1 damage, you get 5 iframes.
