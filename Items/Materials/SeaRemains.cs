741324b0d69759740f6f966081c0425c22ae84c7
CongratsIsTrash <142704082+CongratsIsTrash@users.noreply.github.com>
2025-09-24 19:12:43 -0400

    Reduced Sea Remains sell price due to recipe change

--- Items/Materials/SeaRemains.cs
+++ Items/Materials/SeaRemains.cs
@@ -20,7 +20,7 @@ namespace CalamityMod.Items.Materials
             Item.width = 30;
             Item.height = 24;
             Item.maxStack = Item.CommonMaxStack;
-            Item.value = Item.sellPrice(silver: 36);
+            Item.value = Item.sellPrice(silver: 6);
             Item.rare = ItemRarityID.Green;
         }
         public override void AddRecipes()
