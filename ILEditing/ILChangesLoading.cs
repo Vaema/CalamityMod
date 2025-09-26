0f1f661b84982fc87074443cea2549abd3dae72b
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-09-26 15:27:38 -0400

    Proper Lava Slime config work
    Now properly removes lava drop via an IL edit instead of adding a separate Lava Slime NPC clone and suppressing vanilla Lava Slime spawns (did not work)
    Note that this works by replacing the Remix world check because I honestly have no idea how to directly load the config through IL instructions

--- ILEditing/ILChangesLoading.cs
+++ ILEditing/ILChangesLoading.cs
@@ -189,6 +189,7 @@ namespace CalamityMod.ILEditing
 
             // Removal of vanilla stupidity
             IL_Player.StatusFromNPC += RemoveExpertBrainRandomDebuffs;
+            IL_NPC.VanillaHitEffect += PreventLavaSlimeLavaDrop;
             IL_NPC.StrikeNPC_HitInfo_bool_bool += LetDetonatingBubblesTakeDamage;
             IL_Player.ItemCheck_EmitUseVisuals += MakeMagmaStoneFireGauntletDustToggleable;
             IL_Projectile.EmitEnchantmentVisualsAt += MakeMagmaStoneFireGauntletProjectileDustToggleable;
