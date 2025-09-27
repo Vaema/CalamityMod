c768100f3bf88e6aceb8c1bb9386b04ada71821d
Doze-Zoze <dozezoze@gmail.com>
2025-09-26 19:52:41 -0500

    Void Eater Marionette rework

--- Buffs/Summon/VoidEaterMarionetteBuff.cs
+++ Buffs/Summon/VoidEaterMarionetteBuff.cs
@@ -5,7 +5,7 @@ namespace CalamityMod.Buffs.Summon
 {
     public class VoidEaterMarionetteBuff : BaseSummonBuff
     {
-        protected override int MinionProjectileType => ModContent.ProjectileType<VoidEaterMarionetteHead>();
+        protected override int MinionProjectileType => ModContent.ProjectileType<VoidEaterMarionetteProjectile>();
 
         protected override ref bool MinionBool => ref BuffModdedOwner.hasVoidEaterMarionette;
     }
--- Items/Weapons/Summon/VoidEaterMarionette.cs
+++ Items/Weapons/Summon/VoidEaterMarionette.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using CalamityMod.Buffs.Summon;
 using CalamityMod.Items.Weapons.Rogue;
 using CalamityMod.NPCs.DevourerofGods;
 using CalamityMod.Projectiles.Summon;
 using CalamityMod.Rarities;
+using Microsoft.CodeAnalysis.Operations;
 using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
@@ -19,8 +21,11 @@ namespace CalamityMod.Items.Weapons.Summon
     {
         public new string LocalizationCategory => "Items.Weapons.Summon";
 
+        public bool FocusFetch = false;
+
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
+            tooltips.FindAndReplace("ff00ff", Utils.Hex3(DevourerofGodsHead.SpecialMoveColor));
             tooltips.FindAndReplace("ff00ff", Utils.Hex3(DevourerofGodsHead.SpecialMoveColor));
             tooltips.FindAndReplace("ff00ff", Utils.Hex3(DevourerofGodsHead.SpecialMoveColor));
         }
@@ -43,102 +48,38 @@ namespace CalamityMod.Items.Weapons.Summon
             Item.UseSound = SoundID.Item113;
             Item.autoReuse = true;
             Item.buffType = ModContent.BuffType<VoidEaterMarionetteBuff>();
-            Item.shoot = ModContent.ProjectileType<VoidEaterMarionetteHead>();
+            Item.shoot = ModContent.ProjectileType<VoidEaterMarionetteProjectile>();
             Item.DamageType = DamageClass.Summon;
+            Item.shootSpeed = 8;
         }
-        public override bool CanUseItem(Player player)
+        public override bool CanRightClick()
         {
-            float neededSlots = 1;
-            float foundSlotsCount = 0;
-            foreach (Projectile p in Main.ActiveProjectiles)
-            {
-                if (p.minion && p.owner == player.whoAmI)
-                {
-                    foundSlotsCount += p.minionSlots;
-                    if (foundSlotsCount + neededSlots > player.maxMinions)
-                    {
-                        return false;
-                    }
-                }
-            }
+            if (!Main.keyState.PressingShift())
+                return false;
             return true;
         }
-
-        public static void SummonBaseMechworm(IEntitySource source, int damage, int baseDamage, float knockback, Player owner, out int tailIndex)
+        public override void RightClick(Player player)
         {
-            tailIndex = -1;
-            if (Main.myPlayer != owner.whoAmI)
-                return;
-
-            Vector2 mouse = owner.ClampedMouseWorld();
-
-            int curr = Projectile.NewProjectile(source, mouse, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteHead>(), damage, knockback, owner.whoAmI, 0f, 0f);
-            if (Main.projectile.IndexInRange(curr))
-                Main.projectile[curr].originalDamage = baseDamage;
-            curr = Projectile.NewProjectile(source, mouse, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteBody>(), damage, knockback, owner.whoAmI, Main.projectile[curr].identity, 0f);
-            if (Main.projectile.IndexInRange(curr))
-                Main.projectile[curr].originalDamage = baseDamage;
-            int prev = curr;
-            curr = Projectile.NewProjectile(source, mouse, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteBody>(), damage, knockback, owner.whoAmI, Main.projectile[curr].identity, 0f);
-            if (Main.projectile.IndexInRange(curr))
-                Main.projectile[curr].originalDamage = baseDamage;
-            Main.projectile[prev].localAI[1] = curr;
-            prev = curr;
-            curr = Projectile.NewProjectile(source, mouse, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteTail>(), damage, knockback, owner.whoAmI, Main.projectile[curr].identity, 0f);
-            if (Main.projectile.IndexInRange(curr))
-                Main.projectile[curr].originalDamage = baseDamage;
-            Main.projectile[prev].localAI[1] = curr;
-
-            tailIndex = curr;
+            if (player.ownedProjectileCounts[Item.shoot] > 0)
+            {
+                var p = Main.projectile.First(x => x.active && x.type == Item.shoot && x.owner == player.whoAmI);
+                p.ModProjectile<VoidEaterMarionetteProjectile>().FocusOnFetching = !p.ModProjectile<VoidEaterMarionetteProjectile>().FocusOnFetching;
+                p.netUpdate = true;
+            }
         }
-
-        public static void AddSegmentToMechworm(IEntitySource source, int tailIndex, int damage, int baseDamage, float knockback, Player owner)
-        {
-            if (Main.myPlayer != owner.whoAmI)
-                return;
-
-            Vector2 spawnPosition = Main.projectile[tailIndex].Center;
-            Projectile tailAheadSegment = Main.projectile.Take(Main.maxProjectiles).FirstOrDefault(proj => VoidEaterMarionetteBody.SameIdentity(proj, owner.whoAmI, (int)Main.projectile[tailIndex].ai[0]));
-            int body = Projectile.NewProjectile(source, spawnPosition, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteBody>(), damage, knockback, owner.whoAmI, tailAheadSegment.identity, 0f);
-            int body2 = body;
-            body = Projectile.NewProjectile(source, spawnPosition, Vector2.Zero, ModContent.ProjectileType<VoidEaterMarionetteBody>(), damage, knockback, owner.whoAmI, Main.projectile[body].identity, 0f);
-
-            var m = Main.projectileIdentity;
-            Main.projectile[tailIndex].ai[0] = Main.projectile[body].identity;
-            Main.projectile[tailIndex].netUpdate = true;
-            if (Main.projectile.IndexInRange(body))
-                Main.projectile[body].originalDamage = baseDamage;
-            if (Main.projectile.IndexInRange(body2))
-                Main.projectile[body2].originalDamage = baseDamage;
-        }
-
+        public override bool ConsumeItem(Player player) => false;
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            player.AddBuff(Item.buffType, 2);
-            int head = -1;
-            int tail = -1;
-            foreach (Projectile p in Main.ActiveProjectiles)
+            if (player.ownedProjectileCounts[type] > 0)
             {
-                if (p.owner == Main.myPlayer)
-                {
-                    if (head == -1 && p.type == ModContent.ProjectileType<VoidEaterMarionetteHead>())
-                    {
-                        head = p.whoAmI;
-                    }
-                    if (tail == -1 && p.type == ModContent.ProjectileType<VoidEaterMarionetteTail>())
-                    {
-                        tail = p.whoAmI;
-                    }
-                    if (head != -1 && tail != -1)
-                    {
-                        break;
-                    }
-                }
+                var p = Main.projectile.First(x => x.active && x.type == type && x.owner == player.whoAmI);
+                p.ai[0]++;
+                p.netUpdate = true;
+                return false;
             }
-            if (head == -1 && tail == -1)
-                SummonBaseMechworm(source, damage, Item.damage, knockback, player, out _);
-            else if (head != -1 && tail != -1)
-                AddSegmentToMechworm(source, tail, damage, Item.damage, knockback, player);
+            position = Main.MouseWorld;
+            var worm = Projectile.NewProjectileDirect(source, position, velocity, type, damage, knockback, player.whoAmI,2);
+            Projectile.NewProjectile(source, position, Vector2.Zero, ModContent.ProjectileType<DoGWeaponTeleportRift>(), 0, 0, player.whoAmI);
             return false;
         }
     }
