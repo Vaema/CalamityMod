9c9a47c534cce413c7840fd07ff7491c7fbff9ee
CongratsIsTrash <142704082+CongratsIsTrash@users.noreply.github.com>
2025-10-06 18:56:25 -0400

    Actually lemme make it same as Marrow

--- NPCs/CalamityGlobalNPCLoot.cs
+++ NPCs/CalamityGlobalNPCLoot.cs
@@ -181,9 +181,9 @@ namespace CalamityMod.NPCs
                     break;
 
                 // Armored Skeleton
-                // Beam Sword @ 3.33% (0.67% in Vanilla)
+                // Beam Sword @ 2.5% (0.67% in Vanilla)
                 case NPCID.ArmoredSkeleton:
-                    npcLoot.ChangeDropRate(ItemID.BeamSword, 1, 30);
+                    npcLoot.ChangeDropRate(ItemID.BeamSword, 1, 40);
                     break;
 
                 // Black Recluse
