6a2081515d2edb52805b99d61ef41a899b1ca9d1
Xhack <72313282+energykid@users.noreply.github.com>
2025-10-12 18:37:11 -0400

    Base attack animation & beginnings

--- Items/Weapons/Melee/AmidiasTrident.cs
+++ Items/Weapons/Melee/AmidiasTrident.cs
@@ -1,17 +1,23 @@
-﻿using CalamityMod.Projectiles.Melee.Spears;
+﻿using CalamityMod.Items.BaseItems;
+using CalamityMod.Projectiles.Melee.Spears;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Weapons.Melee
 {
-    public class AmidiasTrident : ModItem, ILocalizedModType
+    public class AmidiasTrident : CustomUseProjItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Weapons.Melee";
         public override void SetStaticDefaults()
         {
             ItemID.Sets.Spears[Type] = true;
         }
+
+        public static readonly int BaseAttackMeleeDamage = 20;
+        public static readonly int BaseAttackProjectileDamage = 10;
+        public static readonly int SecondaryAttackMeleeDamage = 25;
+        public static readonly int SecondaryAttackProjectileDamage = 15;
 
         public override void SetDefaults()
         {
@@ -20,20 +26,21 @@ namespace CalamityMod.Items.Weapons.Melee
             Item.damage = 12;
             Item.DamageType = DamageClass.Melee;
             Item.noMelee = true;
-            Item.useTurn = true;
             Item.noUseGraphic = true;
-            Item.useAnimation = 17;
+            Item.useAnimation = 35;
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.useTime = 17;
+            Item.useTime = 35;
             Item.knockBack = 4.5f;
-            Item.UseSound = SoundID.Item1;
             Item.autoReuse = true;
             Item.value = CalamityGlobalItem.RarityGreenBuyPrice;
             Item.rare = ItemRarityID.Green;
             Item.shoot = ModContent.ProjectileType<AmidiasTridentProj>();
-            Item.shootSpeed = 6f;
         }
 
-        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
+        }
+
     }
 }
--- Projectiles/Melee/Spears/AmidiasTridentProj.cs
+++ Projectiles/Melee/Spears/AmidiasTridentProj.cs
@@ -1,21 +1,28 @@
-﻿using System;
-using CalamityMod.Items.Weapons.Melee;
+﻿using CalamityMod.Items.Weapons.Melee;
 using CalamityMod.Projectiles.BaseProjectiles;
 using Terraria;
-using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using CalamityMod.NPCs;
+using ReLogic.Content;
+using Microsoft.Xna.Framework.Graphics;
+using CalamityMod.Particles;
+using Terraria.Audio;
+using Terraria.ID;
 
 namespace CalamityMod.Projectiles.Melee.Spears
 {
-    public class AmidiasTridentProj : BaseSpearProjectile
+    [PierceResistException]
+    public class AmidiasTridentProj : BaseCustomUseStyleProjectile
     {
+        public override int AssignedItemID => ModContent.ItemType<AmidiasTrident>();
         public override LocalizedText DisplayName => CalamityUtils.GetItemName<AmidiasTrident>();
         public override void SetDefaults()
         {
-            Projectile.width = Projectile.height = 28;
+            Projectile.width = Projectile.height = 50;
             Projectile.DamageType = DamageClass.Melee;
-            Projectile.timeLeft = 90;
+            Projectile.timeLeft = 2;
             Projectile.friendly = true;
             Projectile.hostile = false;
             Projectile.tileCollide = false;
@@ -26,13 +33,79 @@ namespace CalamityMod.Projectiles.Melee.Spears
             Projectile.usesIDStaticNPCImmunity = true;
             Projectile.idStaticNPCHitCooldown = 10;
         }
-
-        public override float InitialSpeed => 3f;
-        public override float ReelbackSpeed => 1f;
-        public override float ForwardSpeed => 0.75f;
-        public override Action<Projectile> EffectBeforeReelback => (proj) =>
+        float rot = 0f;
+        float glow = 0f;
+        public override void ModifyDamageHitbox(ref Rectangle hitbox)
         {
-            Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity * 0.8f, ModContent.ProjectileType<AmidiasWhirlpool>(), Projectile.damage, Projectile.knockBack * 0.85f, Projectile.owner, 0f, 0f);
-        };
+            Vector2 cen = Projectile.Center + new Vector2(45, 0).RotatedBy(rot);
+            hitbox = new Rectangle((int)cen.X - 40, (int)cen.Y - 40, 80, 80);
+        }
+        public override void WhenSpawned()
+        {
+            Projectile.ai[0] = -20;
+            rot = Owner.DirectionTo(Owner.Calamity().mouseWorld).ToRotation();
+        }
+        public override void UseStyle()
+        {
+            switch (Owner.altFunctionUse)
+            {
+                case 2: // Alt attack
+                    break;
+                
+
+                default: // Main attack
+                    if (AnimationProgress < 6)
+                    {
+                        Projectile.damage = AmidiasTrident.BaseAttackMeleeDamage;
+                        if (AnimationProgress == 2)
+                        {
+                            SoundEngine.PlaySound(SoundID.Item1, Owner.Center);
+                            for (int i = 0; i < 6; i++)
+                            {
+                                CustomSpark p = new CustomSpark(Projectile.Center + new Vector2(20, Main.rand.NextFloat(-10, 10)).RotatedBy(rot), Owner.DirectionTo(AbsolutePosition) * 10, "CalamityMod/Particles/ThinEndedLine", false, 5, 1f, Color.AliceBlue.MultiplyRGBA(new(1f, 1f, 1f, 0.5f)), new Vector2(0.2f, 1.2f));
+                                GeneralParticleHandler.SpawnParticle(p);
+                            }
+                        }
+                        Projectile.ai[0] = MathHelper.Lerp(Projectile.ai[0], 92, 0.3f);
+                        glow = MathHelper.Lerp(glow, 1f, 0.3f);
+                    }
+                    else
+                    {
+                        Projectile.damage = 0;
+                        glow = MathHelper.Lerp(glow, 0f, 0.12f);
+                        if (AnimationProgress < 12)
+                        {
+                            Projectile.ai[0] = MathHelper.Lerp(Projectile.ai[0], 65, 0.3f);
+                        }
+                        else
+                        {
+                            rot = rot.AngleLerp(Owner.DirectionTo(Owner.Calamity().mouseWorld).ToRotation(), 0.3f);
+                            Projectile.ai[0] = MathHelper.Lerp(Projectile.ai[0], 10, 0.1f);
+                        }
+                    }
+
+                    Projectile.rotation = rot + MathHelper.ToRadians(135f);
+                    AbsolutePosition = Owner.Center + new Vector2(Projectile.ai[0], 0).RotatedBy(rot);
+                    break;
+            }
+        }
+        public static readonly Asset<Texture2D> StabTexture = ModContent.Request<Texture2D>("CalamityMod/Particles/LargeSpark");
+        public static readonly Asset<Texture2D> SwingTexture = ModContent.Request<Texture2D>("CalamityMod/Particles/CircularSmearFire3");
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Asset<Texture2D> tex = ModContent.Request<Texture2D>("CalamityMod/Particles/GlowBlade");
+
+            int f = (int)(tex.Height() * glow);
+            Main.EntitySpriteDraw(tex.Value, Projectile.Center - Main.screenPosition + new Vector2(-60 + (f * 0.03f), 0).RotatedBy(rot), new Rectangle(0, 0, tex.Width(), f), Color.DeepSkyBlue.MultiplyRGBA(new(glow, glow, glow, 0f)), rot + MathHelper.PiOver2, new Vector2(tex.Width() / 2, tex.Height()), new Vector2(0.03f), SpriteEffects.None);
+
+            return base.PreDraw(ref lightColor);
+        }
+        public override void AI()
+        {
+            base.AI();
+
+            Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation + MathHelper.ToRadians(135f));
+            Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation + MathHelper.ToRadians(135f));
+        }
     }
 }
