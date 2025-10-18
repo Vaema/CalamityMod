c6d2fc70bf9646d1f85ae1e1c1f2c2faccb2a7f2
Altixal <73398287+Altixal@users.noreply.github.com>
2025-10-18 14:25:26 +0700

    banner localization cleanup

--- Items/Placeables/Banners/SandProwlerBanner.cs
+++ Items/Placeables/Banners/SandProwlerBanner.cs
@@ -1,5 +1,8 @@
-﻿namespace CalamityMod.Items.Placeables.Banners
+﻿using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Placeables.Banners
 {
+    [LegacyName("SeaSerpentBanner")]
     public class SandProwlerBanner : BaseBanner
     {
         public override int BannerTileStyle => 106;
