fe6ee478730dc44439fd9169c08f56b7f49864b9
Doze-Zoze <dozezoze@gmail.com>
2025-10-23 10:51:24 -0500

    BaseWormNPC update

--- NPCs/BaseWormNPC.cs
+++ NPCs/BaseWormNPC.cs
@@ -195,6 +195,9 @@ namespace CalamityMod.NPCs
                         rot = rot.RotatedBy(goalSegmentRotOffsets[i] * (mirror ? HeadNPC.NPC.velocity.X.DirectionalSign() : 1));
                         var dir = HeadNPC.Segments[i].Center.DirectionFrom(pos1);
                         HeadNPC.Segments[i].Center = pos1 + Vector2.Lerp(dir, -(rot), segmentRigidity) * dist;
+                        float rotationOffset = Vector2.Lerp(dir, -(rot), segmentRigidity).ToRotation();
+                        float finalOffset = (-rot).ToRotation().AngleLerp(rotationOffset, 0);
+                        HeadNPC.Segments[i].Center = pos1 + finalOffset.ToRotationVector2() * dist;
                         HeadNPC.Segments[i].velocity = HeadNPC.Segments[i].Center.DirectionTo(pos1);
                         HeadNPC.Segments[i].rotation = HeadNPC.Segments[i].velocity.ToRotation() + MathHelper.PiOver2;
                     }
@@ -382,7 +385,9 @@ namespace CalamityMod.NPCs
                 if (internalGlowAssets.Count == 0)
                     for (var i = 0; i < GlowTextures.Count; i++)
                     {
-                        internalGlowAssets.Add(ModContent.Request<Texture2D>(GlowTextures[i]));
+                        if (GlowTextures[i] is not null)
+                            internalGlowAssets.Add(ModContent.Request<Texture2D>(GlowTextures[i]));
+                        else internalGlowAssets.Add(ModContent.Request<Texture2D>("CalamityMod/Projectiles/InvisibleProj"));
                     }
                 return internalGlowAssets;
             }
@@ -410,6 +415,11 @@ namespace CalamityMod.NPCs
         /// How rigid the segment should be when using default segment logic
         /// </summary>
         public float SegmentRigidity = 0.2f;
+
+        /// <summary>
+        /// The max rotational offset from the direction of the previous segment
+        /// </summary>
+        public float SegmentMaxRotation = MathHelper.TwoPi;
 
         /// <summary>
         /// The points used by ExactSegmentLogic to exactly follow the head
@@ -479,7 +489,9 @@ namespace CalamityMod.NPCs
                     nexSegDir = nexSegDir.MoveTowards((aheadSeg.rotation - thisSeg.rotation).ToRotationVector2(), 1f);
                 }
                 thisSeg.rotation = nexSegDir.ToRotation() + MathHelper.PiOver2;
-                thisSeg.Center = aheadSeg.Center - nexSegDir.SafeNormalize(Vector2.Zero) * segmentDistance;
+                float angledif = MathHelper.WrapAngle(thisSeg.rotation - aheadSeg.rotation);
+                thisSeg.rotation = thisSeg.rotation.AngleLerp(aheadSeg.rotation + MathHelper.Clamp(angledif, -SegmentMaxRotation * 0.5f, SegmentMaxRotation * 0.5f),0.25f);
+                thisSeg.Center = aheadSeg.Center - (thisSeg.rotation -MathHelper.PiOver2).ToRotationVector2() * segmentDistance;
 
             }
         }
