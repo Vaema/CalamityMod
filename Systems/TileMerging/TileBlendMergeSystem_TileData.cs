3ce649099c5318fd49fc560908a7b6db2d00e47e
Flowaria <wnsghgj1234@naver.com>
2025-10-24 00:26:36 +0900

    Replacing BlendData storage to Dictionary

---
+++ Systems/TileMerging/TileBlendMergeSystem_TileData.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Systems
+{
+    public sealed partial class TileBlendMergeSystem : ModSystem
+    {
+        // Blending Refs for every tiles
+        private static readonly Dictionary<int, TileBlendingRef[]> _TileBlendingRefs = [];
+
+        public override void ClearWorld()
+        {
+            _TileBlendingRefs.Clear();
+        }
+
+        public static void RemoveBlendingRefData(int tileX, int tileY)
+        {
+            if (!WorldGen.InWorld(tileX, tileY))
+                return;
+
+            var tileIdx = tileX + (Main.tile.Width * tileY);
+            _TileBlendingRefs.Remove(tileIdx);
+        }
+
+        public static void SetBlendingRefData(int tileX, int tileY, TileBlendingRef[] blendingRef)
+        {
+            if (!WorldGen.InWorld(tileX, tileY))
+                return;
+
+            var tileIdx = tileX + (Main.tile.Width * tileY);
+            _TileBlendingRefs[tileIdx] = blendingRef;
+        }
+
+        public static bool TryGetBlendingRefData(int tileX, int tileY, out TileBlendingRef[] blendingRefs)
+        {
+            if (!WorldGen.InWorld(tileX, tileY))
+            {
+                blendingRefs = null;
+                return false;
+            }
+
+            var tileIdx = tileX + (Main.tile.Width * tileY);
+            return _TileBlendingRefs.TryGetValue(tileIdx, out blendingRefs);
+        }
+    }
+}
