1dc7c3b8c6d095ef462513599ca51ae2fbba0330
Altixal <73398287+Altixal@users.noreply.github.com>
2025-11-09 14:58:20 +0700

    Cooldown Rack display config is now enum Updated to expanded config options

--- Enums/SetBonusDoubleTapOptions.cs
+++ Enums/SetBonusDoubleTapOptions.cs
@@ -4,11 +4,8 @@ namespace CalamityMod.Enums
 {
     public enum SetBonusDoubleTapOptions
     {
-        [LabelKey("$Mods.CalamityMod.Configs.CalamityClientConfig.SetBonusDoubleTap.Options.On")]
         On,
-        [LabelKey("$Mods.CalamityMod.Configs.CalamityClientConfig.SetBonusDoubleTap.Options.Auto")]
         Auto,
-        [LabelKey("$Mods.CalamityMod.Configs.CalamityClientConfig.SetBonusDoubleTap.Options.Off")]
         Off
     }
 }
