ca4b7504a00cf7ea1a04082f26ff4cd7f0e1afa1
GinYuH <72181395+GinYuH@users.noreply.github.com>
2025-11-11 21:30:37 -0800

    rubblemaker supports newer piles & added MORE piles

---
+++ Tiles/Rubblemaker/SunkenSea/MediumDunesandPileEcho.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod.Items.Placeables.SunkenSea;
+using CalamityMod.Tiles.SunkenSea.Ambient;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Tiles.Rubblemaker.SunkenSea
+{
+    public class MediumDunesandPileEcho : MediumDunesandPile
+    {
+        public override string Texture => "CalamityMod/Tiles/SunkenSea/Ambient/MediumDunesandPile";
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            RegisterItemDrop(ModContent.ItemType<Dunesand>(), Type);
+            FlexibleTileWand.RubblePlacementMedium.AddVariations(ModContent.ItemType<Dunesand>(), Type, 0);
+        }
+    }
+}
