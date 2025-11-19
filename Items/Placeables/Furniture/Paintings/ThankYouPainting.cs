b6a82d80fe7f190554d46623f0ef0a4ae5b58055
Altixal <73398287+Altixal@users.noreply.github.com>
2025-11-19 14:46:19 +0700

    i got reminded to do this username parity go

--- Items/Placeables/Furniture/Paintings/ThankYouPainting.cs
+++ Items/Placeables/Furniture/Paintings/ThankYouPainting.cs
@@ -50,7 +50,7 @@ namespace CalamityMod.Items.Placeables.Furniture.Paintings
 
         public static IList<string> devList = new List<string>()
         {
-			"Altix",
+			"Altixal",
             "apotofkoolaid",
             "AquaSG",
             "Atalya",
