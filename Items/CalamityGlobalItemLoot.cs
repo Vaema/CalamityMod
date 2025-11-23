7c45ebe31653ef854c916dc96f81929ab9ea7f48
Altixal <73398287+Altixal@users.noreply.github.com>
2025-11-23 11:07:47 +0700

    adjusted rates to match

--- Items/CalamityGlobalItemLoot.cs
+++ Items/CalamityGlobalItemLoot.cs
@@ -555,8 +555,8 @@ namespace CalamityMod.Items
                 ItemDropRule.NotScalingWithLuck(ModContent.ItemType<GrandMarquisBait>(), 3, 3, 7),
                 ItemDropRule.NotScalingWithLuck(ItemID.MasterBait, 1, 3, 7));
 
-        // Non-crafted underground Gold Chest loot for Golden/Titanium Crates @ 20%; Individually 5%
-        private static IItemDropRule UndergroundChestLootRule => new OneFromOptionsNotScaledWithLuckDropRule(5, 1,
+        // Non-crafted underground Gold Chest loot for Golden/Titanium Crates @ 25%; Individually 5%
+        private static IItemDropRule UndergroundChestLootRule => new OneFromOptionsNotScaledWithLuckDropRule(4, 1,
                 ItemID.FlareGun,
                 ItemID.Mace,
                 ItemID.BandofRegeneration,
