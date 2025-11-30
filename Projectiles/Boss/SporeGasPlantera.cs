3a33f7cda8f2d26d059d666bc23d7c2941fd19d1
Stephen Thiebaud <xxlennygamerxx@gmail.com>
2025-11-29 23:01:22 -0600

    feat: load projectile assets for spore gas system upon request

--- Projectiles/Boss/SporeGasPlantera.cs
+++ Projectiles/Boss/SporeGasPlantera.cs
@@ -18,12 +18,6 @@ namespace CalamityMod.Projectiles.Boss
         {
             ProjectileID.Sets.TrailCacheLength[Type] = 2;
             ProjectileID.Sets.TrailingMode[Type] = 0;
-
-            if (!Main.dedServ)
-            {
-                Main.instance.LoadProjectile(ProjectileID.SporeGas2);
-                Main.instance.LoadProjectile(ProjectileID.SporeGas3);
-            }
         }
 
         public override void SetDefaults()
@@ -94,9 +88,11 @@ namespace CalamityMod.Projectiles.Boss
                 case 0:
                     break;
                 case 1:
+                    Main.instance.LoadProjectile(ProjectileID.SporeGas2);
                     texture = TextureAssets.Projectile[ProjectileID.SporeGas2].Value;
                     break;
                 case 2:
+                    Main.instance.LoadProjectile(ProjectileID.SporeGas3);
                     texture = TextureAssets.Projectile[ProjectileID.SporeGas3].Value;
                     break;
                 default:
