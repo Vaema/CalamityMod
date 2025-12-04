73011d1cf88fd4814b8b2634d611e875cb573cc1
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2025-12-04 00:35:10 -0500

    Turned essence fish into quest fish
    Essences are now obtained from crates (Havoc -> Brimstone, Eleum -> Boreal, Sunlight -> Azure)

--- CalPlayer/CalamityPlayerFishing.cs
+++ CalPlayer/CalamityPlayerFishing.cs
@@ -148,7 +148,7 @@ namespace CalamityMod.CalPlayer
                         itemDrop = ModContent.ItemType<Brimlish>();
                     else if (questFish == ModContent.ItemType<Slurpfish>() && attempt.uncommon)
                         itemDrop = ModContent.ItemType<Slurpfish>();
-                    else if (grabBagFish && Main.hardMode)
+                    else if (questFish == ModContent.ItemType<Havocfish>() && attempt.uncommon)
                         itemDrop = ModContent.ItemType<Havocfish>();
                     else if (attempt.rare)
                     {
@@ -272,6 +272,12 @@ namespace CalamityMod.CalPlayer
                 }
             }
 
+            // Quest fish
+            if (sky && questFish == ModContent.ItemType<SunbeamFish>() && attempt.uncommon)
+                itemDrop = ModContent.ItemType<SunbeamFish>();
+            if (Player.ZoneSnow && questFish == ModContent.ItemType<FishofEleum>() && attempt.uncommon)
+                itemDrop = ModContent.ItemType<FishofEleum>();
+
             if (grabBagFish)
             {
                 if (surface && Main.bloodMoon)
@@ -287,10 +293,8 @@ namespace CalamityMod.CalPlayer
                         itemDrop = ModContent.ItemType<FishofNight>();
                     else if (Player.ZoneHallow && cavern && Main.rand.NextBool())
                         itemDrop = ModContent.ItemType<FishofLight>();
-                    else if (Player.ZoneSnow)
-                        itemDrop = ModContent.ItemType<FishofEleum>();
                     else if (sky)
-                        itemDrop = Main.rand.NextBool() ? ModContent.ItemType<SunbeamFish>() : ModContent.ItemType<FishofFlight>();
+                        itemDrop = ModContent.ItemType<FishofFlight>();
                 }
             }
 
