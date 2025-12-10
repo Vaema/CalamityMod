f0aed44fd8b55f82f398d3bf5b6937d80c9521c9
Flowaria <wnsghgj1234@naver.com>
2025-12-11 01:33:51 +0900

    Cache GrayscaleTexture Util Instance

--- Tiles/DraedonStructures/CagedLights/MiniAgedCinderlight.cs
+++ Tiles/DraedonStructures/CagedLights/MiniAgedCinderlight.cs
@@ -1,24 +1,21 @@
-﻿using CalamityMod.Items.Placeables.DraedonStructures.CagedLights;
+﻿using CalamityMod.ExtraTextures.GreyscaleGradients;
+using CalamityMod.Items.Placeables.DraedonStructures.CagedLights;
+using CalamityMod.Sounds;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
 using Terraria.DataStructures;
 using Terraria.Enums;
+using Terraria.ID;
+using Terraria.ModLoader;
 using Terraria.ObjectData;
-using Microsoft.Xna.Framework.Graphics;
-using CalamityMod.Sounds;
 
 namespace CalamityMod.Tiles.DraedonStructures.CagedLights
 {
     public class MiniAgedCinderlight : ModTile
     {
-        internal static GrayscaleTexture1D PulseGradient;
-
         public override void SetStaticDefaults()
         {
-            PulseGradient = new("CalamityMod/Tiles/Plates/CinderplatePulse");
-
             Main.tileLighted[Type] = true;
             Main.tileNoFail[Type] = true;
             Main.tileFrameImportant[Type] = true;
@@ -75,7 +72,7 @@ namespace CalamityMod.Tiles.DraedonStructures.CagedLights
 
         public override void PostDraw(int i, int j, SpriteBatch spriteBatch)
         {
-            float brightness = PulseGradient.GetRepeat((int)Main.GameUpdateCount);
+            float brightness = GreyscaleGradient.CinderplatePulse.GetRepeat((int)Main.GameUpdateCount);
             brightness = MathHelper.Clamp(brightness, 0.2f, 0.6f);
 
             Lighting.AddLight(new Vector2(i * 16, j * 16), 252f / 255f * brightness, 184f / 255f * brightness, 29f / 255f * brightness);
