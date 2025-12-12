79fccca75653eb8eed4c0fad95b00180fbaa4b76
Flowaria <wnsghgj1234@naver.com>
2025-12-12 10:52:34 +0900

    Gemcorns are now allowed to placed on few ModTile

---
+++ Systems/Mechanic/GemcornValidGroundSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityMod.Systems;
+
+internal sealed class GemcornValidGroundSystem : ModSystem
+{
+    public static TileObjectData GemSaplingData => TileObjectData.GetTileData(TileID.GemSaplings, 0);
+
+    public int[] ValidGemcornGrounds = [];
+
+    public override void PostSetupContent()
+    {
+        var tiles = CalamityMod.Instance.GetContent<ModTile>()
+            .Where(tile => WorldGen.GemTreeGroundTest(tile.Type))
+            .Select(tile => (int)tile.Type)
+            .ToArray();
+
+        ValidGemcornGrounds = [.. tiles];
+
+        var gemSaplingData = GemSaplingData;
+        gemSaplingData.AnchorValidTiles = [.. gemSaplingData.AnchorValidTiles, .. ValidGemcornGrounds];
+    }
+
+    public override void Unload()
+    {
+        var gemSaplingData = GemSaplingData;
+        gemSaplingData.AnchorValidTiles = [.. gemSaplingData.AnchorValidTiles.Where(type => !ValidGemcornGrounds.Contains(type))];
+    }
+}
