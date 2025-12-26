47d3613feb5fd531c1d7a4834ba816e412dbb571
Ozzatron <spazeglitch@gmail.com>
2025-12-26 12:54:07 -0500

    Replace donor NPC name for GrayAeternum upon request

--- NPCs/CalamityGlobalTownNPC.cs
+++ NPCs/CalamityGlobalTownNPC.cs
@@ -270,7 +270,6 @@ namespace CalamityMod.NPCs
         private static readonly string[] TaxCollectorNames =
         [
             "Emmett",
-            "Casino King Gray", // <@!555512087711973390> (eternalgrayson)
         ];
         private static readonly string[] TravelingMerchantNames =
         [
@@ -380,7 +379,10 @@ namespace CalamityMod.NPCs
             "Milo", // <@!401849201597874179> (maskedmilo)
             "Octo", // <@!796112889353994281> (octolinggrimm)
         ];
-        private static readonly string[] TownCatSiameseNames = null;
+        private static readonly string[] TownCatSiameseNames =
+        [
+            "Conductor", // <@!555512087711973390> (grayaeternum)
+        ];
         private static readonly string[] TownCatBlackNames =
         [
             "Bear", // <@!183424826407518208> (lilac_vrt_olligoci)
