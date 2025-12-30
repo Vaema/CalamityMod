c3d02a953c2a80450e762d89ac98eba561c2d9aa
Big E <evanmorrison42@gmail.com>
2025-12-30 18:21:18 -0500

    + Added InPunctuationDelay

--- UI/DialogueDisplay/DialogueDisplayUI.cs
+++ UI/DialogueDisplay/DialogueDisplayUI.cs
@@ -678,13 +678,10 @@ namespace CalamityMod.UI.DialogueDisplay
                 }
 
                 int textDelay = DialoguePage.TextDelay;
-                if (DialoguePage.TextDelay != -1)
-                    textDelay = DialoguePage.TextDelay;
+                int inPunctuationDelay = DialoguePage.InPunctuationDelay;
 
                 if (DialoguePage.Event != null && !DialoguePage.Event.IsOver)
-                {
                     DialoguePage.Event.UpdateEvent();
-                }
 
                 if (textIndex < Text.Length)
                 {
@@ -694,6 +691,8 @@ namespace CalamityMod.UI.DialogueDisplay
                     {
                         if (TextTimer == 0)
                         {
+                            bool shouldApplyDelay = false;
+
                             if (!lockDelay)
                             {
                                 char currentChar = Text[textIndex];
@@ -708,18 +707,7 @@ namespace CalamityMod.UI.DialogueDisplay
                                         data = value;
                                 }
 
-                                UnicodeCategory category = char.GetUnicodeCategory(currentChar);
-                                bool shouldApplyDelay = false;
-
-                                if (category >= UnicodeCategory.ConnectorPunctuation && category <= UnicodeCategory.OtherPunctuation)
-                                {
-                                    bool hasLetterBefore = textIndex > 0 && char.IsLetter(Text[textIndex - 1]);
-                                    bool hasLetterAfter = textIndex < Text.Length - 1 && char.IsLetter(Text[textIndex + 1]);
-                                    bool isMidWord = hasLetterBefore && hasLetterAfter;
-
-                                    if (!isMidWord)
-                                        shouldApplyDelay = true;
-                                }
+                                shouldApplyDelay = IsStoppingPunctuation(currentChar, textIndex == 0 ? null : Text[textIndex - 1], textIndex == Text.Length - 1 ? null : Text[textIndex + 1]);
 
                                 if (shouldApplyDelay)
                                 {
@@ -737,7 +725,9 @@ namespace CalamityMod.UI.DialogueDisplay
                                 storedDelay = (int)(pause * 60);
                         }
 
-                        delay = ((Text[textIndex] == ' ' || Text[textIndex] == '\n') && storedDelay > 0 ? textDelay + storedDelay : textDelay);
+                        int delayToUse = (textIndex < Text.Length - 1 && IsPunctuation(Text[textIndex + 1])) ? inPunctuationDelay : textDelay;
+
+                        delay = ((Text[textIndex] == ' ' || Text[textIndex] == '\n') && storedDelay > 0 ? delayToUse + storedDelay : delayToUse);
 
                         if (loopCounter == 0)
                             TextTimer++;
@@ -900,6 +890,27 @@ namespace CalamityMod.UI.DialogueDisplay
             #endregion
 
             DisplayEffects.PostDraw(spriteBatch, pageTop, TextSize, DialogueTimer, SwitchCounter);
+        }
+    
+        private static bool IsStoppingPunctuation(char current, char? before, char? after)
+        {
+            if (IsPunctuation(current))
+            {
+                bool hasLetterBefore = before.HasValue && char.IsLetter(before.Value);
+                bool hasLetterAfter = after.HasValue && char.IsLetter(after.Value);
+                bool isMidWord = hasLetterBefore && hasLetterAfter;
+
+                if (!isMidWord)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsPunctuation(char c)
+        {
+            UnicodeCategory category = char.GetUnicodeCategory(c);
+            return category >= UnicodeCategory.ConnectorPunctuation && category <= UnicodeCategory.OtherPunctuation;
         }
     }
 
@@ -1186,6 +1197,7 @@ namespace CalamityMod.UI.DialogueDisplay
         public Alignment AlignType { get; init; }
 
         public int TextDelay { get; init; }
+        public int InPunctuationDelay { get; init; }
         public PunctuationData BasePunctuationDelay { get; init; }
         public int PunctuationDelayCap { get; init; }
         public Dictionary<string, PunctuationData> PunctuationDelays { get; init; }
@@ -1196,7 +1208,7 @@ namespace CalamityMod.UI.DialogueDisplay
         public int Revision { get; init; }
 
         [JsonConstructor]
-        public DialogueTextData(DialoguePage[] Pages, int Page = 0, string DefaultColor = null, string DefaultSpeaker = null, int DefaultScale = 1, Alignment AlignType = 0, int TextDelay = 3, PunctuationData BasePunctuationDelay = null, int PunctuationDelayCap = 60, Dictionary<string, PunctuationData> PunctuationDelays = null)
+        public DialogueTextData(DialoguePage[] Pages, int Page = 0, string DefaultColor = null, string DefaultSpeaker = null, int DefaultScale = 1, Alignment AlignType = 0, int TextDelay = 3, int InPunctuationDelay = -1, PunctuationData BasePunctuationDelay = null, int PunctuationDelayCap = 60, Dictionary<string, PunctuationData> PunctuationDelays = null)
         {
             this.Pages = Pages;
             this.Page = Page;
@@ -1204,6 +1216,7 @@ namespace CalamityMod.UI.DialogueDisplay
             this.DefaultSpeaker = DefaultSpeaker;
             this.DefaultScale = DefaultScale;
             this.TextDelay = TextDelay;
+            this.InPunctuationDelay = InPunctuationDelay == -1 ? TextDelay : InPunctuationDelay;
             this.BasePunctuationDelay = BasePunctuationDelay ?? new();
             this.PunctuationDelayCap = PunctuationDelayCap;
             this.PunctuationDelays = PunctuationDelays ?? [];
@@ -1217,6 +1230,8 @@ namespace CalamityMod.UI.DialogueDisplay
                     p.TextScale = this.DefaultScale;
                 if (p.TextDelay == -1)
                     p.TextDelay = this.TextDelay;
+                if (p.InPunctuationDelay == -1)
+                    p.InPunctuationDelay = this.InPunctuationDelay;
                 if (p.AlignType == Alignment.None)
                     p.AlignType = this.AlignType;
                 p.BasePunctuationDelay ??= this.BasePunctuationDelay;
@@ -1239,8 +1254,8 @@ namespace CalamityMod.UI.DialogueDisplay
         public int TextScale { get; set; } = -1;
         public Alignment AlignType { get; set; } = Alignment.None;
 
-
         public int TextDelay { get; set; } = -1;
+        public int InPunctuationDelay { get; set; } = -1;
         public PunctuationData BasePunctuationDelay { get; set; } = null;
         public int PunctuationDelayCap { get; set; } = -1;
         public Dictionary<string, PunctuationData> PunctuationDelays { get; set; } = null;
