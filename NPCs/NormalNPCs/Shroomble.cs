e5bacceabbef236d08c3d4f221bad0b5464545a2
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-01-01 16:12:28 -0500

    Shroomble drops a single Mushroom

--- NPCs/NormalNPCs/Shroomble.cs
+++ NPCs/NormalNPCs/Shroomble.cs
@@ -153,7 +153,7 @@ namespace CalamityMod.NPCs.NormalNPCs
             }
         }
 
-        public override void ModifyNPCLoot(NPCLoot npcLoot) => npcLoot.Add(ItemID.Mushroom, 1, 1, 4);
+        public override void ModifyNPCLoot(NPCLoot npcLoot) => npcLoot.Add(ItemID.Mushroom);
 
         public override void HitEffect(NPC.HitInfo hit)
         {
