50e03aa48b55c61f03e569fa4907a57a40c37857
Ozzatron <spazeglitch@gmail.com>
2026-01-05 02:43:33 -0500

    Mechanical improvements to the Onyx Excavator Drill. 1. Mount now actually pickaxes tiles using your best pickaxe. 2. Mount fully mimics pickaxe stats, including reforges for use time. 3. Mount can now dig downwards. Slightly adjusted horizontal digging behavior. 4. Full rewrite to the drilling logic to utilize vanilla codepaths. 5. Abyss tiles are no longer on its blacklist.
    This is a staggering and completely intended nerf to Onyx Excavator Drill.

--- Items/Mounts/OnyxExcavator.cs
+++ Items/Mounts/OnyxExcavator.cs
@@ -5,17 +5,36 @@ using CalamityMod.Tiles.Abyss;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-//using Terraria.GameContent.Achievements;
 using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace CalamityMod.Items.Mounts
 {
+    // TODO:
+    // - don't render the player's arm
+    // - don't allow items to be used at all (like vanilla DCU)
+    // - drill animation speed is independent of mount movement speed and always maxed when picking at least one tile
+    // - separate animation for the drill coming out of the bottom when it digs down
+    // - player head should not bob up and down
     public class OnyxExcavator : ModMount
     {
+        public static List<int> OnyxExcavatorImmuneTiles = null;
+        
         public override void SetStaticDefaults()
         {
+            // 05JAN2026: Ozzatron: Onyx Excavator blacklist is now public and modifiable in a semi-sane way.
+            // Also removed the Abyss tiles from the blacklist because now that the drill respects tile durability, it's fine.
+            OnyxExcavatorImmuneTiles =
+            [
+                // ModContent.TileType<AbyssGravel>(),
+                // ModContent.TileType<PyreMantle>(),
+                // ModContent.TileType<PyreMantleMolten>(),
+                // ModContent.TileType<Voidstone>(),
+                TileID.DemonAltar,
+                TileID.ElderCrystalStand,
+            ];
+
             MountData.spawnDust = 109;
             MountData.spawnDustNoGravity = true;
             MountData.buff = ModContent.BuffType<OnyxExcavatorBuff>();
@@ -69,6 +88,8 @@ namespace CalamityMod.Items.Mounts
             }
         }
 
+        public override void Unload() => OnyxExcavatorImmuneTiles = null;
+
         public override bool UpdateFrame(Player mountedPlayer, int state, Vector2 velocity)
         {
             bool speed = Math.Abs(velocity.X) > mountedPlayer.mount.RunSpeed / 2f;
@@ -95,83 +116,136 @@ namespace CalamityMod.Items.Mounts
             return true;
         }
 
+        /// <summary>
+        /// Attempts to wield the Onyx Excavator to drill the tile at the given position.
+        /// </summary>
+        /// <param name="player">The player using the Onyx Excavator.</param>
+        /// <param name="targetPos">The position of the target tile.</param>
+        /// <param name="pickPower">The power of the player's best pickaxe.</param>
+        /// <returns>Whether the Onyx Excavator successfully mined this tile.</returns>
+        private static bool OnyxExcavateTile(Player player, Point targetPos, int pickPower)
+        {
+            int x = targetPos.X;
+            int y = targetPos.Y;
+            Tile tile = CalamityUtils.ParanoidTileRetrieval(x, y);
+
+            // Startup exclusion checks.
+            if (!tile.HasTile || player.noBuilding || Main.tileContainer[tile.TileType])
+                return false;
+            if (OnyxExcavatorImmuneTiles.Contains(tile.TileType))
+                return false;
+
+            // This function contains all CanKillTile checks and manipulates AchievementsHelper.CurrentlyMining for us.
+            // It also sends appropriate packets in multiplayer.
+            player.PickTile(x, y, pickPower);
+            return true;
+        }
+
         public override void UseAbility(Player player, Vector2 mousePosition, bool toggleOn)
         {
-            if (Main.mouseLeft && !player.mouseInterface && !Main.blockMouse)
+            // Only evaluate drilling on the drill-owning client
+            if (Main.myPlayer != player.whoAmI)
+                return;
+
+            // Any drilling requires holding LMB.
+            bool holdingMouse = Main.mouseLeft && !player.mouseInterface && !Main.blockMouse;
+            if (!holdingMouse)
+                return;
+
+            // Drilling horizontally takes precedence, but if no controls are input, the player will not dig.
+            bool canDrillHorizontally = player.controlLeft || player.controlRight;
+            bool canDrillDown = player.controlDown;
+            if (!canDrillHorizontally && !canDrillDown)
+                return;
+
+            // Onyx Excavator Drill assumes you have an unreforged Copper Pickaxe if you have no pickaxes.
+            Item bestPick = player.GetBestPick() ?? ContentSamples.ItemsByType[ItemID.CopperPickaxe];
+            int pickPower = bestPick.pick;
+            int digCadence = bestPick.useTime;
+
+            // If the cadence does not match the use time of the pickaxe that is being mimicked, do nothing.
+            var cgp = player.Calamity();
+            if (cgp.universalFrameTimer % (ulong)digCadence != 0)
+                return;
+
+            Point[] drillTargets = null;
+
+            // Drilling horizontally takes precedence.
+            // If you want to drill straight down, you need to hold down without left or right.
+            if (canDrillHorizontally)
             {
-                if (Main.myPlayer == player.whoAmI)
-                {
-                    int highestPickPower = player.GetBestPickPower();
+                // Determine mining direction based on the player's facing.
+                // At extremely low velocities, the drill intentionally drills at closer positions.
+                // This is so if you're parked against a wall it doesn't drill through the first layer from a standstill.
+                float xVel = player.velocity.X;
+                float absVel = Math.Abs(xVel);
+                int direction = absVel < 0.1f ? player.direction : Math.Sign(xVel);
+                int xTileOffset = absVel > 0.5f ? 2 : 1;
 
-                    float direction = Math.Sign(player.velocity.X);
-                    if (Math.Abs(direction) < 0.1f)
-                        direction = player.direction == 1 ? 0.1f : -0.1f;
-                    float yOffset = 12f;
-                    float xOffset = 40f;
-                    Vector2 source = player.Center + new Vector2(direction * xOffset, yOffset);
-                    int mineHeight = 3;
-                    int xRight = (int)(source.X / 16 - mineHeight);
-                    int xLeft = (int)(source.X / 16 + mineHeight);
-                    int yTop = (int)(source.Y / 16 - mineHeight);
-                    int yBottom = (int)(source.Y / 16 + mineHeight);
-                    if (xRight < 0)
-                    {
-                        xRight = 0;
-                    }
-                    if (xLeft > Main.maxTilesX)
-                    {
-                        xLeft = Main.maxTilesX;
-                    }
-                    if (yTop < 0)
-                    {
-                        yTop = 0;
-                    }
-                    if (yBottom > Main.maxTilesY)
-                    {
-                        yBottom = Main.maxTilesY;
-                    }
-                    //AchievementsHelper.CurrentlyMining = true;
-                    for (int x = xRight; x <= xLeft; x++)
-                    {
-                        for (int y = yTop; y <= yBottom; y++)
-                        {
-                            source.Y -= 2f;
-                            Vector2 tileVec = new Vector2(Math.Abs(x - source.X / 16f), Math.Abs(y - source.Y / 16f));
-                            float tileDist = tileVec.Length();
-                            Tile tile = CalamityUtils.ParanoidTileRetrieval(x, y);
+                // These are offset so they're always correct when getting floored by integer division
+                int playerLeadingEdgeX = direction == -1 ? player.Hitbox.Left + 2 : player.Hitbox.Right - 2;
+                int bottomTileCenterY = player.Hitbox.Bottom - 8;
+                int drillLeadingEdgeX = playerLeadingEdgeX + direction * 16 * xTileOffset;
 
-                            int pickReq = tile.GetRequiredPickPower(x, y);
+                // Forms the following pattern:
+                // |X
+                // |XX
+                // |XX
+                // |X
+                Vector2 drillOrigin     = new(drillLeadingEdgeX, bottomTileCenterY);
+                Vector2 drillOneUp      = drillOrigin + new Vector2(0f, -16f);
+                Vector2 drillTwoUp      = drillOrigin + new Vector2(0f, -32f);
+                Vector2 drillThreeUp    = drillOrigin + new Vector2(0f, -48f);
+                Vector2 drillFrontLower = drillOrigin + new Vector2(16f * direction, -16f);
+                Vector2 drillFrontUpper = drillOrigin + new Vector2(16f * direction, -32f);
 
-                            if (tileDist < mineHeight)
-                            {
-                                bool t = 1 == 1;
-                                bool f = 1 == 2;
-                                bool canBreakTileCheck = TileLoader.CanKillTile(x, y, tile.TileType, ref t) && TileLoader.CanKillTile(x, y, tile.TileType, ref f);
-                                List<int> tileExcludeList = new List<int>()
-                                {
-                                    ModContent.TileType<AbyssGravel>(),
-                                    ModContent.TileType<PyreMantle>(),
-                                    ModContent.TileType<PyreMantleMolten>(),
-                                    ModContent.TileType<Voidstone>(),
-                                    TileID.DemonAltar,
-                                    TileID.ElderCrystalStand
-                                };
-
-                                if (tile.HasTile && !player.noBuilding && !Main.tileContainer[tile.TileType] &&
-                                    tileExcludeList.TrueForAll(z => tile.TileType != z) && pickReq <= highestPickPower && canBreakTileCheck)
-                                {
-                                    WorldGen.KillTile(x, y, false, false, false);
-                                    if (!Main.tile[x, y].HasTile && Main.netMode != NetmodeID.SinglePlayer)
-                                    {
-                                        NetMessage.SendData(MessageID.TileManipulation, -1, -1, null, 0, x, y, 0f, 0, 0, 0);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    //AchievementsHelper.CurrentlyMining = false;
-                }
+                drillTargets =
+                [
+                    drillOrigin.ToSafeTileCoordinates(),
+                    drillOneUp.ToSafeTileCoordinates(),
+                    drillTwoUp.ToSafeTileCoordinates(),
+                    drillThreeUp.ToSafeTileCoordinates(),
+                    drillFrontLower.ToSafeTileCoordinates(),
+                    drillFrontUpper.ToSafeTileCoordinates(),
+                ];
             }
+
+            else if(canDrillDown)
+            {
+                // Nothing needs to be considered. Simply dig straight down. Players are 2 tiles wide so this will always work.
+                int playerLeftEdgeX = player.Hitbox.Left + 2;
+                int playerRightEdgeX = player.Hitbox.Right - 2;
+                int drillLeadingEdgeY = player.Hitbox.Bottom + 8;
+
+                // Two horizontal lines of four tiles each, covering the bottom part of the mount and the floor beneath it.
+                Vector2 drillLeftNear  = new(playerLeftEdgeX, drillLeadingEdgeY);
+                Vector2 drillRightNear = new(playerRightEdgeX, drillLeadingEdgeY);
+                Vector2 drillLeftFar   = drillLeftNear + new Vector2(-16f, 0f);
+                Vector2 drillRightFar  = drillRightNear + new Vector2(16f, 0f);
+                Vector2 mountLeftNear  = drillLeftNear + new Vector2(0f, -16f);
+                Vector2 mountRightNear = drillRightNear + new Vector2(0f, -16f);
+                Vector2 mountLeftFar   = drillLeftFar + new Vector2(0f, -16f);
+                Vector2 mountRightFar  = drillRightFar + new Vector2(0f, -16f);
+
+                drillTargets =
+                [
+                    mountLeftNear.ToSafeTileCoordinates(),
+                    mountRightNear.ToSafeTileCoordinates(),
+                    mountLeftFar.ToSafeTileCoordinates(),
+                    mountRightFar.ToSafeTileCoordinates(),
+                    drillLeftNear.ToSafeTileCoordinates(),
+                    drillRightNear.ToSafeTileCoordinates(),
+                    drillLeftFar.ToSafeTileCoordinates(),
+                    drillRightFar.ToSafeTileCoordinates(),
+                ];
+            }
+
+            if (drillTargets is null)
+                return;
+
+            // Execute drilling on all intended target tiles.
+            foreach (Point p in drillTargets)
+                OnyxExcavateTile(player, p, pickPower);
         }
     }
 }
