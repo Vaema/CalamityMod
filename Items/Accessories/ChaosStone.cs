72b5ae8251e30a815e88e61fc33adffd58dd265c
doze-zoze <dozezoze@gmail.com>
2026-01-25 18:17:12 -0600

    chaos stone balance, tooltips

--- Items/Accessories/ChaosStone.cs
+++ Items/Accessories/ChaosStone.cs
@@ -8,6 +8,7 @@ namespace CalamityMod.Items.Accessories
     public class ChaosStone : ModItem, ILocalizedModType
     {
         public new string LocalizationCategory => "Items.Accessories";
+                var dmgAmount = Main.LocalPlayer.statManaMax2 == 0 ? 0 : ((Main.LocalPlayer.statManaMax2 / 100f) * LostRegenPer100Mana) * 0.5f;
         public override void SetStaticDefaults()
         {
             Main.RegisterItemAnimation(Item.type, new DrawAnimationVertical(8, 7));
