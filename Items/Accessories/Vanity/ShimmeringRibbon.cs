d7436a72d7bf66f81c7b78244f926ec7b27d6437
Big E <evanmorrison42@gmail.com>
2026-02-09 15:59:01 -0500

    + Sagi is also here??

---
+++ Items/Accessories/Vanity/ShimmeringRibbon.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalamityMod.Items.BaseItems;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Accessories.Vanity
+{
+    internal class ShimmeringRibbon : TransformationAccessory, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Accessories";
+
+        public override (EquipType, string, string)[] EquipSlots =>
+        [
+            (EquipType.Head, "Charlotte", null),
+            (EquipType.Body, "Charlotte", null),
+            (EquipType.Legs, "Charlotte", null),
+        ];
+
+        public override void SetDefaults()
+        {
+            Item.width = 30;
+            Item.height = 32;
+            Item.accessory = true;
+            Item.vanity = true;
+            Item.rare = ItemRarityID.Blue;
+            Item.value = CalamityGlobalItem.RarityBlueBuyPrice;
+            Item.Calamity().devItem = true;
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.Silk, 5).
+                AddIngredient(ItemID.FallenStar, 1).
+                AddTile(TileID.Loom).
+                Register();
+        }
+    }
+}
