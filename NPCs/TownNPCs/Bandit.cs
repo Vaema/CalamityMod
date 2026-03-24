fb1611bcbc1a88ebc5048c23b3148595350e7487
Ozzatron <spazeglitch@gmail.com>
2026-03-23 21:45:57 -0400

    Add Yuhru to Bandit

--- NPCs/TownNPCs/Bandit.cs
+++ NPCs/TownNPCs/Bandit.cs
@@ -121,6 +121,7 @@ namespace CalamityMod.NPCs.TownNPCs
             "Ariallis", // <@!518231218806980609> (ariallis)
             "Shade", // <@!613133259563466755> (shade__storm)
             "Orion", // <@!119164557657636865> (canacx)
+            "Yuhru", // <@!194276252452913154> (yuhru_)
 
             // Original names
             this.GetLocalizedValue("Name.Laura"),
