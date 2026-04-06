36d10b493f6d616a023fd6333671ed2f211dab7e
Nathanael Overton <nsoverton@gmail.com>
2026-04-06 04:28:31 -0400

    Silly tooltip easter egg

---
+++ Buffs/Potions/SwinesWrathBuff.cs
@@ -0,0 +1,17 @@
+﻿using CalamityMod.Items.Potions;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Buffs.Potions
+{
+    public class SwinesWrathBuff : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.debuff[Type] = true;
+            Main.pvpBuff[Type] = true;
+            Main.buffNoSave[Type] = false;
+        }
+    }
+}
--- Items/Potions/TheConcoction.cs
+++ Items/Potions/TheConcoction.cs
@@ -1,4 +1,6 @@
-﻿using CalamityMod.Items.Materials;
+﻿using System.Collections.Generic;
+using System.Linq;
+using CalamityMod.Items.Materials;
 using CalamityMod.Rarities;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -9,11 +11,14 @@ namespace CalamityMod.Items.Potions
 {
     public class TheConcoction : ModItem, ILocalizedModType
     {
+        private static int hoverTimer = 0;
         public new string LocalizationCategory => "Items.Potions";
+
         public override void SetStaticDefaults()
         {
             Item.ResearchUnlockCount = 30;
-            ItemID.Sets.DrinkParticleColors[Type] = new Color[3] {
+            ItemID.Sets.DrinkParticleColors[Type] = new Color[3] 
+            {
                 new Color(255, 190, 250),
                 new Color(255, 225, 183),
                 new Color(246, 34, 79)
@@ -25,6 +30,27 @@ namespace CalamityMod.Items.Potions
             Item.DefaultToHealingPotion(50, 56, 500);
             Item.value = Item.sellPrice(silver: 60);
             Item.rare = ItemRarityID.Green;
+        }
+
+        // Display different text for the first 8 frames of hovering over the item's tooltip
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+            if (Main.HoverItem?.type != Type)
+            {
+                hoverTimer = 0;
+                return;
+            }
+
+            hoverTimer++;
+
+            if (hoverTimer <= 8)
+            {
+                TooltipLine healLine = tooltips.FirstOrDefault(x => x.Mod == "Terraria" && x.Name == "HealLife");
+                if (healLine != null)
+                {
+                    healLine.Text = this.GetLocalization("EasterEggText").Value;
+                }
+            }
         }
     }
 }
