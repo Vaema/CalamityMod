2d39f274771597ca3af637674a3a27356191bdd3
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-04-13 22:50:34 -0400

    Humble beginnings
    No functionality

---
+++ Items/Placeables/Furniture/GluttonyBlender.cs
@@ -0,0 +1,20 @@
+﻿using CalamityMod.Tiles.Furniture;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Placeables.Furniture
+{
+    public class GluttonyBlender : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables";
+        public override string Texture => "Terraria/Images/Item_2193"; //Placeholder
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<GluttonyBlenderTile>());
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.buyPrice(gold: 5);
+        }
+    }
+}
---
+++ Tiles/Furniture/GluttonyBlender.cs
@@ -0,0 +1,31 @@
+﻿using CalamityMod.Items.Placeables.Furniture;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+
+namespace CalamityMod.Tiles.Furniture
+{
+    public class GluttonyBlenderTile : ModTile
+    {
+        public override string Texture => "CalamityMod/Tiles/Furniture/GluttonyPlaceholder";
+
+        public override void SetStaticDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileLavaDeath[Type] = true;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.addTile(Type);
+            AddMapEntry(new Color(128, 128, 128), CalamityUtils.GetItemName<GluttonyBlender>());
+        }
+
+        public override bool CreateDust(int i, int j, ref int type) => false;
+
+        public override void SetDrawPositions(int i, int j, ref int width, ref int offsetY, ref int height, ref short tileFrameX, ref short tileFrameY) => offsetY = 2;
+
+        /*public override void AnimateTile(ref int frame, ref int frameCounter)
+        {
+            base.AnimateTile(ref frame, ref frameCounter);
+        }*/
+    }
+}
