64a16cde39408185aa0b4f56ff38a42583d5692a
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-04-14 23:04:30 -0400

    This is blenderslop, you only like it because it blends
    A starting attempt at creating functionality

--- Items/Placeables/Furniture/GluttonyBlender.cs
+++ Items/Placeables/Furniture/GluttonyBlender.cs
@@ -17,4 +17,35 @@ namespace CalamityMod.Items.Placeables.Furniture
             Item.value = Item.buyPrice(gold: 5);
         }
     }
+
+    public class DeliciousSlop : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Potions";
+        public override string Texture => "CalamityMod/Items/Potions/Food/DeliciousMeat"; //Placeholder
+
+        public override void SetDefaults()
+        {
+            Item.DefaultToFood(32, 30, BuffID.WellFed3, CalamityUtils.MinutesToFrames(60));
+            Item.rare = ItemRarityID.Orange;
+        }
+    }
+
+    public class DisgustingSlop : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Misc";
+        public override string Texture => "CalamityMod/Items/Tools/DisgustingMeat"; //Placeholder
+
+        public override void SetStaticDefaults()
+        {
+            ItemID.Sets.ExtractinatorMode[Type] = ItemID.OldShoe;
+        }
+        public override void SetDefaults()
+        {
+            Item.width = 26;
+            Item.height = 36;
+            Item.maxStack = Item.CommonMaxStack;
+            Item.rare = ItemRarityID.Gray;
+            Item.MakeUsableWithChlorophyteExtractinator();
+        }
+    }
 }
--- Tiles/Furniture/GluttonyBlender.cs
+++ Tiles/Furniture/GluttonyBlender.cs
@@ -1,6 +1,12 @@
 ﻿using CalamityMod.Items.Placeables.Furniture;
+using CalamityMod.Tiles.DraedonStructures;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
@@ -8,6 +14,9 @@ namespace CalamityMod.Tiles.Furniture
 {
     public class GluttonyBlenderTile : ModTile
     {
+        public const int Width = 3;
+        public const int Height = 3;
+
         public override string Texture => "CalamityMod/Tiles/Furniture/GluttonyPlaceholder";
 
         public override void SetStaticDefaults()
@@ -15,7 +24,10 @@ namespace CalamityMod.Tiles.Furniture
             Main.tileFrameImportant[Type] = true;
             Main.tileLavaDeath[Type] = true;
             TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            ModTileEntity entity = ModContent.GetInstance<GluttonyBlenderTE>();
+            TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(entity.Hook_AfterPlacement, -1, 0, true);
             TileObjectData.addTile(Type);
+
             AddMapEntry(new Color(128, 128, 128), CalamityUtils.GetItemName<GluttonyBlender>());
         }
 
@@ -27,5 +39,120 @@ namespace CalamityMod.Tiles.Furniture
         {
             base.AnimateTile(ref frame, ref frameCounter);
         }*/
+
+        public override void MouseOver(int i, int j)
+        {
+            Player p = Main.LocalPlayer;
+            // You shouldn't be able to put the slop back into the blender
+            if (BuffID.Sets.IsWellFed[p.HeldItem.buffType] && p.HeldItem.type != ModContent.ItemType<DeliciousSlop>())
+            {
+                p.noThrow = 2;
+                p.cursorItemIconEnabled = true;
+                p.cursorItemIconID = p.HeldItem.type;
+            }
+        }
+    }
+
+    public class GluttonyBlenderTE : ModTileEntity
+    {
+        public Vector2 BlenderTop => Position.ToWorldCoordinates(8 * GluttonyBlenderTile.Width, 0f);
+
+        public override bool IsTileValidForEntity(int x, int y)
+        {
+            Tile tile = Main.tile[x, y];
+            return tile.HasTile && tile.TileType == ModContent.TileType<GluttonyBlenderTile>() && tile.TileFrameX == 0 && tile.TileFrameY == 0;
+        }
+
+        public override int Hook_AfterPlacement(int i, int j, int type, int style, int direction, int alternate)
+        {
+            if (Main.netMode == NetmodeID.MultiplayerClient)
+            {
+                NetMessage.SendTileSquare(Main.myPlayer, i, j, GluttonyBlenderTile.Width, GluttonyBlenderTile.Height);
+                NetMessage.SendData(MessageID.TileEntityPlacement, -1, -1, null, i, j, Type);
+                return -1;
+            }
+            return Place(i, j);
+        }
+    }
+
+    public class GluttonyBlenderGlobalItem : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+
+        internal bool FromGluttonyBlender = false;
+
+        public override bool CanUseItem(Item item, Player player)
+        {
+            Point mouseTile = Main.MouseWorld.ToTileCoordinates();
+            if (Main.LocalPlayer.whoAmI == player.whoAmI && BuffID.Sets.IsWellFed[item.buffType] &&
+                CalamityUtils.FindTileEntity<GluttonyBlenderTE>(mouseTile.X, mouseTile.Y, GluttonyBlenderTile.Width, GluttonyBlenderTile.Height) != null &&
+                player.IsInTileInteractionRange(mouseTile.X, mouseTile.Y, TileReachCheckSettings.Simple))
+            {
+                player.ApplyItemAnimation(item);
+                return false;
+            }
+            return base.CanUseItem(item, player);
+        }
+        public override bool? UseItem(Item item, Player player)
+        {
+            Main.NewText("g");
+
+            if (Main.LocalPlayer.whoAmI != player.whoAmI)
+                return null;
+
+            if (!BuffID.Sets.IsWellFed[item.buffType])
+                return null;
+
+            Point mouseTile = Main.MouseWorld.ToTileCoordinates();
+            GluttonyBlenderTE entity = CalamityUtils.FindTileEntity<GluttonyBlenderTE>(mouseTile.X, mouseTile.Y, GluttonyBlenderTile.Width, GluttonyBlenderTile.Height);
+            if (entity == null)
+                return null;
+
+            if (!player.IsInTileInteractionRange(mouseTile.X, mouseTile.Y, TileReachCheckSettings.Simple))
+                return null;
+
+            // Velocity doesn't get used for this thing, so it's used here to pass in the position of the top of the blender
+            Projectile.NewProjectile(player.GetSource_TileInteraction(mouseTile.X, mouseTile.Y), player.Center, entity.BlenderTop, ModContent.ProjectileType<GluttonyBlenderAnimation>(),
+                0, 0f, player.whoAmI, item.type);
+            //player.ApplyItemTime(item, 1, false);
+
+            return false;
+        }
+    }
+
+    public class GluttonyBlenderAnimation : ModProjectile, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Projectiles.Typeless";
+        public override string Texture => "CalamityMod/Projectiles/InvisibleProj";
+
+        public ref float ItemType => ref Projectile.ai[0];
+
+        public override void SetDefaults()
+        {
+            Projectile.width = Projectile.height = 10;
+            Projectile.tileCollide = false;
+            Projectile.ignoreWater = true;
+            Projectile.timeLeft = 120;
+        }
+
+        public override bool ShouldUpdatePosition() => false;
+        public override void AI()
+        {
+            if (Projectile.timeLeft == 120)
+            {
+                Main.NewText("s");
+                Projectile.ai[1] = Projectile.velocity.X;
+                Projectile.ai[2] = Projectile.velocity.Y;
+                Projectile.velocity = Vector2.Zero;
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Texture2D tex = TextureAssets.Item[(int)ItemType].Value;
+            Rectangle frame = tex.Frame(1, Main.itemAnimations[(int)ItemType].FrameCount, 0, 0);
+            Main.EntitySpriteDraw(tex, Projectile.Center, frame, lightColor, Projectile.rotation, Projectile.Size * 0.5f, Projectile.scale, SpriteEffects.None);
+            return false;
+        }
     }
 }
