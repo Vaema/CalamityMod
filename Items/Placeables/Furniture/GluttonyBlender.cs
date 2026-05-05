2cfe06054c3392f27f60d0dc05d5a2f31b8eaeb8
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-05-04 22:09:08 -0400

    Quality Slop held and plate frames
    + other food consistency stuff

--- Items/Placeables/Furniture/GluttonyBlender.cs
+++ Items/Placeables/Furniture/GluttonyBlender.cs
@@ -1,5 +1,7 @@
 ﻿using CalamityMod.Tiles.Furniture;
+using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -23,9 +25,23 @@ namespace CalamityMod.Items.Placeables.Furniture
         public new string LocalizationCategory => "Items.Potions";
         public override string Texture => "CalamityMod/Items/Potions/Food/QualitySlop";
 
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 5;
+            Main.RegisterItemAnimation(Type, new DrawAnimationVertical(int.MaxValue, 3));
+            ItemID.Sets.FoodParticleColors[Type] = [
+                new Color(183, 65, 68),
+                new Color(167, 116, 36),
+                new Color(175, 87, 190)
+            ];
+            ItemID.Sets.IsFood[Type] = true;
+            ItemID.Sets.ShimmerTransformToItem[Type] = ItemID.Ambrosia;
+        }
+
         public override void SetDefaults()
         {
             Item.DefaultToFood(32, 30, BuffID.WellFed3, CalamityUtils.MinutesToFrames(60));
+            Item.value = Item.sellPrice(gold: 1);
             Item.rare = ItemRarityID.Orange;
         }
     }
