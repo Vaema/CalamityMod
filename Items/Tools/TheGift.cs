32bbba3074e8e51417c45fe9f5de843a56515ee0
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-05-17 17:52:59 -0400

    Changed Gift's MP syncing logic
    Hopefully this helps relieve MP bugs
    Also changes it to not consume the entire stack when picked up

--- Items/Tools/TheGift.cs
+++ Items/Tools/TheGift.cs
@@ -38,7 +38,7 @@ namespace CalamityMod.Items.Tools
         {
             // Controls actually applying the effect to an NPC
             // None of this should work in Remix because Remix disables happiness
-            if (Item.noGrabDelay <= 0 && !Main.remixWorld)
+            if (Item.noGrabDelay <= 0 && !Main.remixWorld && !Main.dedServ && Item.playerIndexTheItemIsReservedFor == Main.myPlayer)
             {
                 foreach (NPC n in Main.ActiveNPCs)
                 {
@@ -73,11 +73,13 @@ namespace CalamityMod.Items.Tools
                             GeneralParticleHandler.SpawnParticle(arr);
                         }
 
-                        Item.active = false;
-                        Item.type = ItemID.None;
-                        Item.stack = 0;
-                        if (Main.dedServ)
-                            NetMessage.SendData(MessageID.SyncItem, -1, -1, null, Item.whoAmI);
+                        Item.stack--;
+                        if (Item.stack <= 0)
+                        {
+                            Item.active = false;
+                            Item.type = ItemID.None;
+                        }
+                        NetMessage.SendData(MessageID.SyncItem, -1, -1, null, Item.whoAmI);
                         // Make sure it can't apply to multiple NPCs at once
                         break;
                     }
