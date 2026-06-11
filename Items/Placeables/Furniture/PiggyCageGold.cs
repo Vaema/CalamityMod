d4298b480e7618acce19c1ec65b4941a8e8f940a
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-06-11 15:09:58 -0400

    Added Gold Piggy Cage

---
+++ Items/Placeables/Furniture/PiggyCageGold.cs
@@ -0,0 +1,24 @@
+﻿using CalamityMod.Items.Critters;
+using CalamityMod.Tiles.Furniture;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityMod.Items.Placeables.Furniture
+{
+    public class PiggyCageGold : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeables";
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<PiggyCageGoldTile>());
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe().
+                AddIngredient(ItemID.Terrarium).
+                AddIngredient<PiggyGoldItem>().
+                Register();
+        }
+    }
+}
