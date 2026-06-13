48af6d1eadb6667fd2363405fe90ea54541e2328
Karina <gpscorpionreal@gmail.com>
2026-06-13 04:24:10 -0500

    Changes and Balance

--- Balancing/BalancingChangesManager.cs
+++ Balancing/BalancingChangesManager.cs
@@ -290,8 +290,7 @@ namespace CalamityMod.Balancing
             #endregion
 
             #region Old Duke
-            // 20% resist to Time Bolt.
-            NPCSpecificBalancingChanges.Add(new NPCBalancingChange(NPCType<OldDuke>(), new ProjectileResistBalancingRule(0.8f, ProjectileType<TimeBoltKnife>())));
+
             #endregion
 
             #region The Devourer of Gods
@@ -306,15 +305,10 @@ namespace CalamityMod.Balancing
 
             // 20% resist to Valediction's typhoons; will catch Nuclear Fury as well but that doesn't matter.
             NPCSpecificBalancingChanges.AddRange(Bundle(CalamityNPCTypeSets.DevourerOfGods, Do(new ProjectileResistBalancingRule(0.8f, ProjectileType<NuclearFuryProjectile>()))));
-
-            // 15% vulnerability to Time Bolt stealth strikes.
-            NPCSpecificBalancingChanges.AddRange(Bundle(CalamityNPCTypeSets.DevourerOfGods, Do(new StealthStrikeBalancingRule(1.15f, ProjectileType<TimeBoltKnife>()))));
             #endregion The Devourer of Gods
 
             #region Yharon
 
-            // 15% resist to Time Bolt.
-            NPCSpecificBalancingChanges.Add(new NPCBalancingChange(NPCType<Yharon>(), Do(new ProjectileResistBalancingRule(0.85f, ProjectileType<TimeBoltKnife>()))));
             #endregion
 
             #region Exo Mechs: Ares
