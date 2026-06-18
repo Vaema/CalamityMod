cb9d751765cf9f73d392a80701c86f98a1abefd2
CongratsIsTrash <1q2w3e4dasd@gmail.com>
2026-06-18 17:59:53 -0400

    Spadefish, Valkyrie Ray resprites

--- Items/Weapons/Magic/ValkyrieRay.cs
+++ Items/Weapons/Magic/ValkyrieRay.cs
@@ -15,7 +15,7 @@ namespace CalamityMod.Items.Weapons.Magic
         // This math is handled in its holdout projectile, ValkyrieRayStaff.
         public const int ChargeFrames = 24;
         public const int CooldownFrames = 12;
-        public const float GemDistance = 18f;
+        public const float GemDistance = 27f;
         public static readonly Color LightColor = new Color(235, 40, 121);
 
         public override void SetStaticDefaults()
