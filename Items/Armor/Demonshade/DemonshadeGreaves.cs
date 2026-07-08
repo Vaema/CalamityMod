380057095c0631c269fde80212dadf262c680871
sunny-raindrops <snowyqueenofducks@gmail.com>
2026-07-08 11:51:23 -0700

    Demonshade accel toggle

--- Items/Armor/Demonshade/DemonshadeGreaves.cs
+++ Items/Armor/Demonshade/DemonshadeGreaves.cs
@@ -3,6 +3,10 @@ using CalamityMod.Rarities;
 using CalamityMod.Tiles.Furniture.CraftingStations;
 using Terraria;
 using Terraria.ModLoader;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader.IO;
 
 namespace CalamityMod.Items.Armor.Demonshade
 {
@@ -26,10 +30,47 @@ namespace CalamityMod.Items.Armor.Demonshade
             Item.Calamity().devItem = true;
         }
 
+        #region Toggleable Acceleration effect
+
+        bool toggleEnabled = true;
+
+        public override bool CanRightClick() => Main.keyState.PressingShift();
+        public override void RightClick(Player player)
+        {
+            toggleEnabled = !toggleEnabled;
+            Item.NetStateChanged();
+        }
+        public override bool ConsumeItem(Player player) => false;
+        public override void SaveData(TagCompound tag)
+        {
+            tag.Add("toggleEffect", toggleEnabled);
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            toggleEnabled = tag.GetBool("toggleEffect");
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            writer.Write(toggleEnabled);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            toggleEnabled = reader.ReadBoolean();
+        }
+
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            CalamityUtils.DrawInventoryDot(spriteBatch, position, new Vector2(16, 16) * Main.inventoryScale, toggleEnabled);
+        }
+        #endregion
+
         public override void UpdateEquip(Player player)
         {
             var modPlayer = player.Calamity();
-            modPlayer.shadowSpeed = true;
+            modPlayer.shadowSpeed = toggleEnabled;
             player.moveSpeed += MoveSpeedBoost;
         }
 
