24cf78850538de5552fc16ce41690277a9453547
Nathanael Overton <nsoverton@gmail.com>
2026-07-09 18:07:26 -0400

    moved to Audio Toggles

--- CalamityConfig.cs
+++ CalamityConfig.cs
@@ -154,10 +154,6 @@ namespace CalamityMod
         [BackgroundColor(192, 54, 64, 192)]
         [DefaultValue(true)]
         public bool FlightBar { get; set; }
-
-        [BackgroundColor(192, 54, 64, 192)]
-        [DefaultValue(false)]
-        public bool MisophoniaSupport { get; set; }
         #endregion
 
         #region Meter Positions
@@ -248,8 +244,8 @@ namespace CalamityMod
         public float FlightBarPosY { get; set; }
         #endregion
 
-        #region Music Toggles
-        [Header("MusicToggles")]
+        #region Audio Toggles
+        [Header("AudioToggles")]
 
         [BackgroundColor(192, 54, 64, 192)]
         [DefaultValue(true)]
@@ -263,6 +259,9 @@ namespace CalamityMod
         [DefaultValue(false)]
         public bool AbyssLayer3Alt { get; set; }
 
+        [BackgroundColor(192, 54, 64, 192)]
+        [DefaultValue(false)]
+        public bool MisophoniaSupport { get; set; }
         #endregion
 
         #region General Gameplay Changes
